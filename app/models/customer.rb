daad9794ceaf520a877f62fd4c465a300e3707ea
donrestarone <shashikejayatunge@gmail.com>
2021-04-03 16:29:56 -0400

    uncomitted changes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,10 @@
 class Customer < ApplicationRecord
   after_create_commit :create_tenant, :create_cms_site
 
+  def name
+    self.subdomain
+  end
+
   private
   def create_tenant
     Apartment::Tenant.create(self.subdomain)
@@ -11,6 +15,16 @@ class Customer < ApplicationRecord
       identifier: self.subdomain,
       # this is only for local testing
       hostname:   "#{self.subdomain}.lvh.me:3000",
+    )
+    layout = site.layouts.create(
+      label: self.name,
+      identifier: self.name,
+      content: "{{cms:wysiwyg content}}"
+    )
+    layout.pages.create(
+      site_id: site.id,
+      label: self.name,
+      content: "<h3>Hello from #{self.subdomain}</h3>"
     )
   end
 end
