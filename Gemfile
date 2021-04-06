6b3193cee549aa9110459e07c933bb07b93734f5
donrestarone <shashikejayatunge@gmail.com>
2021-04-05 21:01:04 -0400

    add test for sign up

--- Gemfile
+++ Gemfile
@@ -55,7 +55,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
-  gem 'database_cleaner-active_record'
+  gem 'simplecov', require: false, group: :test
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,12 @@
+require 'simplecov'
+SimpleCov.start 'rails'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
