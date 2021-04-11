3e644dfad972f5a29e32d7a8e4920fba20f27937
donrestarone <shashikejayatunge@gmail.com>
2021-04-11 12:59:46 -0400

    start testing subdomain

--- test/models/subdomain_test.rb
+++ test/models/subdomain_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class SubdomainTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @subdomain = subdomains(:public)
+  end
+
+  test "uniqness" do
+    duplicate = Subdomain.new(
+      name: @subdomain.name
+    )
+    refute duplicate.valid?
+  end
+
+  test 'full host' do
+    assert @subdomain.hostname
+  end
 end
