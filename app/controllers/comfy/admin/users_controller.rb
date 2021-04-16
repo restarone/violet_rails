b353762889b0d9bb7a33c5258ef832349e013a93
donrestarone <shashikejayatunge@gmail.com>
2021-04-15 21:17:57 -0400

    basic invitable

--- app/controllers/comfy/admin/users_controller.rb
+++ app/controllers/comfy/admin/users_controller.rb
@@ -1,18 +1,39 @@
 class Comfy::Admin::UsersController < Comfy::Admin::Cms::BaseController
   layout "comfy/admin/cms"
+  
   def index
-
+    @users = User.all
   end
 
   def new
-
   end
 
-  def create
-
+  def invite
+    user = User.invite!(invite_params)
+    if user.persisted?
+      flash.notice = "#{user.email} was invited"
+      redirect_to admin_users_path
+    else
+      flash.alert = "User could not be invited"
+    end
   end
 
   def edit
 
+  end
+
+  def update
+
+  end
+
+
+  def destroy
+  
+  end
+
+  private 
+
+  def invite_params
+    params.require(:user).permit(:email)
   end
 end
--- test/controllers/admin/comfy/users_controller_test.rb
+++ test/controllers/admin/comfy/users_controller_test.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
+
+
 class Comfy::Admin::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @restarone_subdomain = Subdomain.find_by(name: 'restarone')
@@ -9,10 +11,52 @@ class Comfy::Admin::UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "get #index by authorized personnel" do
     sign_in(@user)
-    get users_url(subdomain: @domain)
+    get admin_users_url(subdomain: @domain)
     assert_response :success
     assert_template :index
     assert response.body.include? I18n.t('views.comfy.users.index.header.title')
     assert response.body.include? I18n.t('views.comfy.users.index.header.action')
+  end
+
+  test "deny #index" do
+    get admin_users_url(subdomain: @domain)
+    assert_response :redirect
+  end
+
+  test "#new" do
+    sign_in(@user)
+    get new_admin_user_url(subdomain: @domain)
+    assert_response :success
+  end
+
+  test "#edit" do
+    sign_in(@user)
+    get edit_admin_user_url(subdomain: @domain, id: @user.id)
+    assert_response :success
+  end
+
+  test "#update" do
+    sign_in(@user)
+    patch admin_user_url(subdomain: @domain, id: @user.id)
+    assert_response :success
+  end
+
+  test "#invite" do
+    sign_in(@user)
+    payload = {
+      user: {
+        email: 'testemail@tester.com'
+      }
+    }
+    assert_difference "User.all.size", +1 do      
+      post invite_admin_users_url(subdomain: @domain, params: payload)
+      assert_redirected_to admin_users_url(subdomain: @domain)
+    end
+  end
+
+  test "#destroy" do
+    sign_in(@user)
+    delete admin_user_url(subdomain: @domain, id: @user.id)
+    assert_response :success
   end
 end
