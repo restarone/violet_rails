31c222a5fa49c877365bc269d86c889146624582
donrestarone <shashikejayatunge@gmail.com>
2021-04-19 20:30:27 -0400

    need to fix tests

---
+++ test/controllers/mailbox/message_threads_controller_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class Mailbox::MessageThreadsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:public)
+    @user.update(can_manage_email: true)
+    @user.initialize_mailbox
+    @message_thread = message_threads(:public)
+    @subdomain = subdomains(:public)
+    @restarone_subdomain = Subdomain.find_by(name: 'restarone').name
+    Apartment::Tenant.switch @restarone_subdomain do
+      @unauthorized_user = User.first
+      @unauthorized_user.update(can_manage_email: true)
+    end
+  end
+
+  test "denies #show if not logged in" do
+    get mailbox_message_thread_url(subdomain: @subdomain.name, id: @message_thread.id)
+    assert_response :redirect
+    assert_redirected_to new_user_session_url(subdomain: @subdomain.name)
+  end
+
+  test "denies #show if user doesnt belong to subdomain" do
+    sign_in(@unauthorized_user)
+    get mailbox_message_thread_url(subdomain: @subdomain.name, id: @message_thread.id)
+    assert_response :redirect
+    assert flash.alert
+    assert_redirected_to root_url
+  end
+
+  test "allows #show if logged in" do
+    sign_in(@user)
+    get mailbox_message_thread_url(subdomain: @subdomain.name, id: @message_thread.id)
+    assert_response :success
+  end
+end
