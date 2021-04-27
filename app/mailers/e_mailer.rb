4ed8239fbe437fe5d2f0badd6a937df8b50ff797
donrestarone <shashikejayatunge@gmail.com>
2021-04-26 20:43:20 -0400

    debug

--- app/mailers/e_mailer.rb
+++ app/mailers/e_mailer.rb
@@ -4,7 +4,7 @@ class EMailer < ApplicationMailer
     @message = params[:message]
     @message_thread = params[:message_thread]
     @from = "#{Subdomain.current.name}@#{ENV["APP_HOST"]}"
-    message_id = @message_thread.message_id ? @message_thread.message_id : "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{Rails.application.config.action_mailer.mailgun_settings[:domain]}"
+    message_id = @message_thread.email_message_id ? @message_thread.email_message_id : "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{Rails.application.config.action_mailer.mailgun_settings[:domain]}"
     @message_thread.recipients.each do |recipient|
       mail(
         to: recipient, 
