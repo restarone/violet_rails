2c3dc4e70d6830355bd57e95079fcaa35a48f435
donrestarone <shashikejayatunge@gmail.com>
2021-05-07 13:57:19 -0400

    uncomitted changes

--- db/migrate/20210507120500_add_forum_columns_to_users_table.rb
+++ db/migrate/20210507120500_add_forum_columns_to_users_table.rb
@@ -1,8 +1,8 @@
 class AddForumColumnsToUsersTable < ActiveRecord::Migration[6.1]
   def change
     change_table :users do |t|
-      t.string :name
       # for forum only
+      t.string :name
       t.boolean :moderator
       t.boolean :admin
     end
