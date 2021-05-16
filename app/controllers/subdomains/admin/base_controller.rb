1a8712268868cce43b471a2f588edb3cb885ebfc
donrestarone <shashikejayatunge@gmail.com>
2021-05-16 12:02:41 -0400

    remove unneeded code

--- app/controllers/subdomains/admin/base_controller.rb
+++ app/controllers/subdomains/admin/base_controller.rb
@@ -5,11 +5,9 @@ class Subdomains::Admin::BaseController < Subdomains::BaseController
   private
 
   def ensure_user_belongs_to_subdomain
-    Apartment::Tenant.switch Apartment::Tenant.current do
-      unless User.find_by(email: current_user.email)
-        flash.alert = 'You do not have access to that'
-        redirect_to root_path
-      end
+    unless User.find_by(email: current_user.email)
+      flash.alert = 'You do not have access to that'
+      redirect_to root_path
     end
   end
 end
