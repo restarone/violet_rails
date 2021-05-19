350c665b6cadd6bb4906f86b1a89dda661f51cb7
donrestarone <shashikejayatunge@gmail.com>
2021-05-19 09:00:18 -0400

    remove unneeded migration

--- db/migrate/20210519125651_bootstrap_public_name.rb
+++
@@ -1,7 +0,0 @@
-class BootstrapPublicName < ActiveRecord::Migration[6.1]
-  def change
-    Apartment::Tenant.switch('public') do
-      Subdomain.unsafe_bootstrap_www_subdomain
-    end
-  end
-end
