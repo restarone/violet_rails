ed679a22dca34fab4173be002fcccbcdc1db19c9
donrestarone <shashikejayatunge@gmail.com>
2021-05-19 08:34:32 -0400

    update test make sure that root name and tenant names work

--- test/controllers/content_controller_test.rb
+++ test/controllers/content_controller_test.rb
@@ -3,12 +3,16 @@ require "test_helper"
 class ContentControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:public)
+    @restarone_subdomain = Subdomain.find_by(name: 'restarone').name
+    Apartment::Tenant.switch @restarone_subdomain do
+      @restarone_user = User.find_by(email: 'contact@restarone.com')
+    end
   end
 
   test "should get index" do
     get root_url
     assert_response :success
-    get root_url(subdomain: @user.subdomain)
+    get root_url(subdomain: @restarone_user.subdomain)
     assert_response :success
   end
 end
