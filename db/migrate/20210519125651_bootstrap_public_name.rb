a41fafc661ec773588a379a6b1a1960dcf8418ea
donrestarone <shashikejayatunge@gmail.com>
2021-05-19 08:58:19 -0400

    ubcomitted changes

--- db/migrate/20210519125651_bootstrap_public_name.rb
+++ db/migrate/20210519125651_bootstrap_public_name.rb
@@ -1,4 +1,7 @@
 class BootstrapPublicName < ActiveRecord::Migration[6.1]
   def change
+    Apartment::Tenant.switch('public') do
+      Subdomain.unsafe_bootstrap_www_subdomain
+    end
   end
 end
