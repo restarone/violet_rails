2d4055bf44485dee22bcfd7333e2e0e490f028ea
donrestarone <shashikejayatunge@gmail.com>
2021-05-21 12:38:06 -0400

    lock down comfy cms pages

---
+++ db/migrate/20210521152836_add_private_cms_pages.rb
@@ -0,0 +1,8 @@
+class AddPrivateCmsPages < ActiveRecord::Migration[6.1]
+  def change
+    change_table :comfy_cms_pages do |t|
+      ## makes pages private to users who are signed in + have can_view_restricted_pages: true
+      t.boolean :is_restricted
+    end
+  end
+end
---
+++ test/controllers/admin/comfy/cms/content_controller_test.rb
@@ -0,0 +1,42 @@
+require "test_helper"
+
+class Comfy::Cms::ContentControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @restarone_subdomain = Subdomain.find_by(name: 'restarone').name
+    Apartment::Tenant.switch @restarone_subdomain do
+      @site = Comfy::Cms::Site.first
+      @user = User.first
+      refute @user.can_manage_web
+      @layout = @site.layouts.last
+      @page = @layout.pages.first
+      @page.update(is_restricted: true)
+    end
+  end
+
+  test "get root page" do
+    get root_url
+    assert_response :success
+  end
+
+  test "deny restricted page (redirect to login)" do
+    get root_url(subdomain: @restarone_subdomain)
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "deny restricted page (redirect to admin)" do
+    refute @user.can_view_restricted_pages
+    sign_in(@user)
+    get root_url(subdomain: @restarone_subdomain)
+    assert_response :redirect
+    assert_redirected_to comfy_admin_cms_path
+  end
+
+  test "allow restricted page (if permissioned)" do
+    @user.update(can_view_restricted_pages: true)
+    assert @user.can_view_restricted_pages
+    sign_in(@user)
+    get root_url(subdomain: @restarone_subdomain)
+    assert_response :success
+  end
+end
