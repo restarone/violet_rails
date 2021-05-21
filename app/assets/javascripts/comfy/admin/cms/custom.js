4d62f710f31dbc13387607e08287c5ee23e89f4e
donrestarone <shashikejayatunge@gmail.com>
2021-05-21 10:15:44 -0400

    change language

--- app/assets/javascripts/comfy/admin/cms/custom.js
+++ app/assets/javascripts/comfy/admin/cms/custom.js
@@ -1,7 +1,7 @@
 // Custom JS for the admin area
 
 let subdomain = /:\/\/([^\/]+)/.exec(window.location.href)[1].split('.')[0]
-document.title = `${subdomain} WebAdmin`
+document.title = `${subdomain} Admin`
 if ($('#message_thread_recipients').length) {
   $('select').select2({
     multiple: true,
