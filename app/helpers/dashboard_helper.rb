968553224481ef46da30148552fda9cd1711b2e8
donrestarone <shashikejayatunge@gmail.com>
2021-05-29 12:48:24 -0400

    sneak in a bug fix here too

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,11 @@
 module DashboardHelper
+  def redact_private_urls(url)
+    return if !url
+    exclusions = Subdomain::PRIVATE_URL_PATHS
+    if exclusions.any? {|exclusion| url.include?(exclusion) }
+      "private-system-url-redacted"
+    else
+      url
+    end
+  end
 end
