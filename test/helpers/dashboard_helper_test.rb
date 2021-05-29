3be918612ac8e2b036ca7e81a04f652da8cc9163
donrestarone <shashikejayatunge@gmail.com>
2021-05-29 14:28:15 -0400

    add test

--- test/helpers/dashboard_helper_test.rb
+++ test/helpers/dashboard_helper_test.rb
@@ -4,9 +4,11 @@ class DashboardHelperTest < ActionView::TestCase
   test 'redacts devise user links' do
     url = "http://violet.lvh.me:5250/users/confirmation?confirmation_token=fQg7D1kv1d_12f8U_yDX"
     url_2 = "http://violet.lvh.me:5250/users/confirmation?confirmation_token=fQg7D1kv1d_12f8U_yDX"
+    url_3 = "https://violet.restarone.solutions/users/password/edit?reset_password_token=-Es6rYgrrDssyyzVNnA6"
     url_non_redactable = "http://violet.lvh.me:5250/users/golf"
     assert_equal "private-system-url-redacted" , redact_private_urls(url)
     assert_equal "private-system-url-redacted" , redact_private_urls(url_2)
+    assert_equal "private-system-url-redacted" , redact_private_urls(url_3)
     assert_equal url_non_redactable , redact_private_urls(url_non_redactable)
   end
 end
