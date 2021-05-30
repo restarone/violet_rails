dc25f3dc326de47f8659ccb3df290d2627b760c3
donrestarone <shashikejayatunge@gmail.com>
2021-05-30 12:07:26 -0400

    add preview

--- app/helpers/call_to_actions_helper.rb
+++ app/helpers/call_to_actions_helper.rb
@@ -2,6 +2,7 @@ module CallToActionsHelper
 
   def render_call_to_action(id)
     # usage in cms  {{ cms:helper render_call_to_action, 1 }} here 1 is the id
+    # usage in rails = render_call_to_action @call_to_action.id
     @call_to_action = CallToAction.find_by(id: id)
     if @call_to_action
       @call_to_action_response = @call_to_action.call_to_action_responses.new
