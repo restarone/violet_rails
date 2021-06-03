04d8832ca3b0f8db69d146b86bc6ce1e2fcfef0f
donrestarone <shashikejayatunge@gmail.com>
2021-06-02 22:26:50 -0400

    send editor json to backend

--- app/controllers/comfy/admin/api_namespaces_controller.rb
+++ app/controllers/comfy/admin/api_namespaces_controller.rb
@@ -22,6 +22,7 @@ class Comfy::Admin::ApiNamespacesController < Comfy::Admin::Cms::BaseController
 
   # POST /api_namespaces or /api_namespaces.json
   def create
+    byebug
     @api_namespace = ApiNamespace.new(api_namespace_params)
 
     respond_to do |format|
