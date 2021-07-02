29ef0ecaf9dbb06762f4922dddd429c3677cecf7
donrestarone <shashikejayatunge@gmail.com>
2021-07-01 20:45:40 -0400

    fix migrations where tables were mutated directly without the change method. This could be problematic when rolling back certain changes. better to have the correct syntax in place

--- db/migrate/20210620015322_add_forum_and_blog_controls_to_subdomains.rb
+++ db/migrate/20210620015322_add_forum_and_blog_controls_to_subdomains.rb
@@ -1,6 +1,8 @@
 class AddForumAndBlogControlsToSubdomains < ActiveRecord::Migration[6.1]
-  change_table :subdomains do |t|
-    t.boolean :forum_enabled, default: true
-    t.boolean :blog_enabled, default: true
+  def change
+    change_table :subdomains do |t|
+      t.boolean :forum_enabled, default: true
+      t.boolean :blog_enabled, default: true
+    end
   end
 end
--- db/migrate/20210620024451_add_allow_self_signup_column_to_subdomains.rb
+++ db/migrate/20210620024451_add_allow_self_signup_column_to_subdomains.rb
@@ -1,5 +1,7 @@
 class AddAllowSelfSignupColumnToSubdomains < ActiveRecord::Migration[6.1]
-  change_table :subdomains do |t|
-    t.boolean :allow_user_self_signup, default: true
+  def change
+    change_table :subdomains do |t|
+      t.boolean :allow_user_self_signup, default: true
+    end
   end
 end
