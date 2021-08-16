bf9b0e57a71c1c04cec777531cf192ffa2cd4066
donrestarone <shashikejayatunge@gmail.com>
2021-08-16 17:28:47 -0400

    adjust schedule and set default to never

--- db/migrate/20210815214536_add_ahoy_visit_cleaning_frequency_to_subdomain.rb
+++ db/migrate/20210815214536_add_ahoy_visit_cleaning_frequency_to_subdomain.rb
@@ -1,7 +1,7 @@
 class AddAhoyVisitCleaningFrequencyToSubdomain < ActiveRecord::Migration[6.1]
   def change
     change_table :subdomains do |t|
-      t.string :purge_visits_every, default: "3.months"
+      t.string :purge_visits_every, default: "never"
     end
   end
 end
