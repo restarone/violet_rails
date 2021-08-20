e0504e206f03e4ee71ebf8222e56c16a9302698e
donrestarone <shashikejayatunge@gmail.com>
2021-08-20 19:55:54 -0400

    debug boolean cast + rename handler

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
   def query
     search_q = params[:query]
-    pages = Comfy::Cms::Page.ransack(search_q)
-    render json: pages.result
+    pages = Comfy::Cms::Page.where.not(is_restricted: false, is_published: true).ransack(search_q)
+    respond_to do |format|
+      format.html { redirect_to '/search' }
+      format.js   { render json: pages.result }
+    end
   end
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -3,6 +3,6 @@ module SearchHelper
   def render_search_field(ajax = true)
     # usage in cms  {{ cms:helper render_search_field, false }}
     # usage in rails = render_search_field false
-    render partial: 'comfy/helpers/search/render', locals: { ajax:  ajax }
+    render partial: 'comfy/helpers/search/render', locals: { ajax:  ActiveModel::Type::Boolean.new.cast(ajax) }
   end
 end
