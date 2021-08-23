03e17a09f8dc9119bf9b2f9c0e182956e8b004cf
donrestarone <shashikejayatunge@gmail.com>
2021-08-23 19:58:52 -0400

    uncomitted changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_214536) do
+ActiveRecord::Schema.define(version: 2021_08_23_235033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -251,6 +251,7 @@ ActiveRecord::Schema.define(version: 2021_08_15_214536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_restricted"
+    t.text "preview_content"
     t.index ["is_published"], name: "index_comfy_cms_pages_on_is_published"
     t.index ["parent_id", "position"], name: "index_comfy_cms_pages_on_parent_id_and_position"
     t.index ["site_id", "full_path"], name: "index_comfy_cms_pages_on_site_id_and_full_path"
@@ -411,7 +412,7 @@ ActiveRecord::Schema.define(version: 2021_08_15_214536) do
     t.boolean "blog_enabled", default: true
     t.boolean "allow_user_self_signup", default: true
     t.boolean "forum_is_private", default: false
-    t.string "purge_visits_every", default: "3.months"
+    t.string "purge_visits_every", default: "never"
     t.index ["deleted_at"], name: "index_subdomains_on_deleted_at"
     t.index ["name"], name: "index_subdomains_on_name"
   end
