b150727920797f6fd497d6cd8e2553ae27c9f521
donrestarone <shashikejayatunge@gmail.com>
2021-08-23 06:45:15 -0400

    finalize queryable endpoint

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,15 @@
 class SearchController < ApplicationController
   def query
     search_q = params[:query]
-    matching_fragments = Comfy::Cms::Fragment
-      .where(record_type: "Comfy::Cms::Page")
-      .where("content ILIKE ?", "%#{search_q}%")
-          
-    site_pages = Comfy::Cms::Page
-      .where.not(is_restricted: false, is_published: true)
-      .where(id: matching_fragments.pluck(:record_id))
-
-    respond_to do |format|
-      format.html { redirect_to '/search' }
-      format.json   { render json: site_pages }
-      format.js   { render json: site_pages }
+    if search_q
+      matching_fragments = Comfy::Cms::Fragment
+        .where(record_type: "Comfy::Cms::Page")
+        .where("to_tsvector(comfy_cms_fragments.content) @@ to_tsquery('english', ?)", search_q)
+      site_pages = Comfy::Cms::Page
+      .where(id: matching_fragments.pluck(:record_id), is_restricted: false, is_published: true)
+    else
+      site_pages = { status: 'search parameter not defined', code: 422 }
     end
+    render json: site_pages
   end
 end
