e1ebc9fe20accea2ad53b73de80cd01c39f7103c
donrestarone <shashikejayatunge@gmail.com>
2021-08-27 06:45:14 -0400

    revert back to ilike query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,7 @@ class SearchController < ApplicationController
   def query
     search_q = params[:query]
     if search_q
-      matching_fragments = Comfy::Cms::Fragment
-        .where(record_type: "Comfy::Cms::Page")
-        .where("to_tsvector(comfy_cms_fragments.content) @@ to_tsquery('english', ?)", search_q)
+      matching_fragments = Comfy::Cms::Fragment.where("content ILIKE ?", "%#{search_q}%")
       site_pages = Comfy::Cms::Page
       .where(id: matching_fragments.pluck(:record_id), is_restricted: false, is_published: true)
     else
