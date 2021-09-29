14a3d1861e9f7327644e0e55aee32d3e9287fb6e
pralish <pralishkayastha@gmail.com>
2021-09-29 07:14:05 +0545

    spec(): use appropriate test url

--- test/services/analytics_report_service_test.rb
+++ test/services/analytics_report_service_test.rb
@@ -15,7 +15,7 @@ class AnalyticsReportServiceTest  < ActiveSupport::TestCase
     assert_equal report[:visits][:city]["toronto"], 1
     assert_equal report[:visits][:region]["onterio"], 1
     assert_equal report[:visits][:referring_domain]["localhost"], 1
-    assert_equal report[:visits][:landing_page]["lvh.me:5250"], 1 
+    assert_equal report[:visits][:landing_page]["http://lvh.me:5250/"], 1 
     assert_equal report[:users].count, 1
     assert_equal report[:macros], {users: {total: 2, added: 1}, pages: {total: 1, added: 1}, storage: {total: "0 Bytes", added: "0 Bytes"}}
   end
