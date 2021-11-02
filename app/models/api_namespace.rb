a6d182822a91147220cca4b619f3636eef9059dd
pralish <pralishkayastha@gmail.com>
2021-11-02 17:45:13 +0545

    feat(): create api form if has_form is set to true

--- app/models/api_namespace.rb
+++ app/models/api_namespace.rb
@@ -1,11 +1,38 @@
 class ApiNamespace < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  attr_accessor :has_form
+
+  after_save :update_api_form
   
   has_many :api_resources, dependent: :destroy
   accepts_nested_attributes_for :api_resources
 
-    
+  has_one :api_form
+  accepts_nested_attributes_for :api_form
+
   has_many :api_clients, dependent: :destroy
   accepts_nested_attributes_for :api_clients
+
+  def update_api_form
+    if has_form == '1'
+      if api_form.present? 
+        api_form.update({ properties: form_properties })
+      else
+        create_api_form({ properties: form_properties })
+      end
+    elsif has_form == '0' && api_form.present?
+      api_form.destroy
+    end
+  end
+
+  def form_properties
+    form_hash = {}
+    form_properties = api_form.present? ? api_form.properties : {}
+    JSON.parse(properties).each do |key, _value|
+      form_hash[key] = form_properties[key].present? ? form_properties[key] : { label: key.humanize, placeholder: '', required: false }
+    end
+    form_hash
+  end
 end
