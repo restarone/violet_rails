5ff2e67ec29df7f3147421bea9ef966a7e04e2fc
pralish <pralishkayastha@gmail.com>
2021-11-07 11:49:50 +0545

    feat(): ransack api namespace properties

--- app/models/api_resource.rb
+++ app/models/api_resource.rb
@@ -3,4 +3,8 @@ class ApiResource < ApplicationRecord
 
   has_many :non_primitive_properties, dependent: :destroy
   accepts_nested_attributes_for :non_primitive_properties, allow_destroy: true
+
+  ransacker :properties do |parent|
+    Arel.sql("api_resources.properties::text") 
+  end
 end
