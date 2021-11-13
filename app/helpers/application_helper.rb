e3666a7b3afa961f10ace256c2b3cae22785488b
pralish <pralishkayastha@gmail.com>
2021-11-13 20:48:43 +0545

    feat(): serve file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,12 @@ module ApplicationHelper
       tag.b("#{collection.offset + 1} - #{[collection.per_page * collection.current_page, collection.total_entries].min}") +
       ' of ' + tag.b(collection.total_entries) +
       ' in total'
-end
+  end
+
+  def execute_actions(resource, class_name)
+    api_actions = resource.send(class_name)
+    api_actions.each do |api_action|
+      api_action.execute_action  unless api_action.serve_file? || api_action.redirect?
+    end
+  end
 end
