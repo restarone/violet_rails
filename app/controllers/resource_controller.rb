cca6b9b26836880911477f07d5ccde533caa75d1
pralish <pralishkayastha@gmail.com>
2021-11-15 18:55:29 +0545

    feat(): parse hash fields before saving

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -29,6 +29,12 @@ class ResourceController < ApplicationController
   end
   
   def resource_params
+    # it comes in as a json string which we need to parse into a ruby hash before saving it to the DB 
+    JSON.parse(@api_namespace.properties).each do |key, value|
+      if value.class.to_s == 'Hash'
+        params[:data][:properties][key] = JSON.parse params[:data][:properties][key]
+      end
+    end
     params.require(:data).permit(properties: {}, non_primitive_properties_attributes: [:id, :label, :field_type, :content, :attachment, :_destroy])
   end
 end
