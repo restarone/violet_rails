fbc5b74d441fcfd20c1f19da890bc1757f519249
pralish <pralishkayastha@gmail.com>
2021-11-16 17:56:59 +0545

    feat(): preview img/video before upload

--- app/javascript/packs/comfy/admin/cms.js
+++ app/javascript/packs/comfy/admin/cms.js
@@ -25,3 +25,19 @@ function resetIndex(containerId) {
       });
   }
   
+window.previewFile = function previewFile(event, previewId) {
+    var file = event.target.files[0]
+    var output;
+    if (file && file.type.match(/video/)) {
+      output = $('#' + previewId + '_video');
+      $('#' + previewId + '_img').hide();
+    } else if (file && file.type.match(/image/)) {
+      output = $('#' + previewId + '_img');
+      $('#' + previewId + '_video').hide();
+    }
+    output.show();
+    output.attr('src', URL.createObjectURL(file));
+    output.on('load', function() {
+      URL.revokeObjectURL(output.src)
+    })
+}
