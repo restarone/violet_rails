4e8beec28a46034fc2398e9cbc27a6ea6c50cfad
pralish <pralishkayastha@gmail.com>
2021-11-24 08:58:48 +0545

    chore(): specify tenant before runing rake task

--- app/models/api_form.rb
+++ app/models/api_form.rb
@@ -2,9 +2,9 @@ class ApiForm < ApplicationRecord
   belongs_to :api_namespace
 
   INPUT_TYPE_MAPPING = {
-    free_text: 'free text',
+    free_text: 'free_text',
     number: 'number',
-    regex_pattern: 'REGEX pattern',
+    regex_pattern: 'regex_pattern',
     email: 'email',
     url: 'url',
     date: 'date',
