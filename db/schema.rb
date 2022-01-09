f12ddc71ebbe6bd812f3c0bc011866097b996d32
Pralish Kayastha <pralishkayastha@gmail.com>
2022-01-09 09:10:44 +0545

    merged with master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_020948) do
+ActiveRecord::Schema.define(version: 2021_11_19_063616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
