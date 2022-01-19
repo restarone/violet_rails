3ba75a076c335b74ba91fde6c047dbebfbdf113e
donrestarone <shashikejayatunge@gmail.com>
2022-01-19 07:34:33 -0500

    changes required to use violet rails template for a new project

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,8 +2,8 @@ if Rails.env.production?
   Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
 else
   if Rails.env.development?
-    Sidekiq.configure_server { |c| c.redis = { url: 'redis://solutions_redis:6379/12' } }
+    Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
   else
-    Sidekiq.configure_server { |c| c.redis = { url: 'redis://localhost:6379' } }
+    Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
   end
 end
