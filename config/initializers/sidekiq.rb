b3e124664278e7e1b76515b513df075088aaf9be
donrestarone <shashikejayatunge@gmail.com>
2022-01-19 07:56:24 -0500

    make test files work

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,2 @@
-if Rails.env.production?
-  Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
-else
-  if Rails.env.development?
-    Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
-  else
-    Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
-  end
-end
+Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
+
--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 RAILS_ENV=test
 echo "Setting: RAILS_ENV=$RAILS_ENV"
 
-docker-compose run --rm solutions_test rails test -f --verbose
+docker-compose run --rm test rails test -f --verbose
