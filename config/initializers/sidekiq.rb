efb027bfb5c076e49bc5a69884010957d37f848b
donrestarone <shashikejayatunge@gmail.com>
2022-03-14 19:29:59 -0400

    fix init file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,1 @@
-if Rails.env.production?
-  Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
-else
-  if Rails.env.development?
-    Sidekiq.configure_server { |c| c.redis = { url: 'redis://solutions_redis:6379/12' } }
-  else
-    Sidekiq.configure_server { |c| c.redis = { url: 'redis://localhost:6379' } }
-  end
-end
+Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
