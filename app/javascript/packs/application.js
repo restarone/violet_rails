19bd8ebfb90d2d90a751b5f868c922726f2b9f64
donrestarone <shashikejayatunge@gmail.co>
2022-04-12 19:52:23 -0400

    uncomitted changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,4 +22,7 @@ Turbolinks.start()
 require("jquery")
 require("./trix")
 require("./select2")
-require("./common")
+require("./common")// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
