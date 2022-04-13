61939931544047aa583ebce429bbb09d6552535a
donrestarone <shashikejayatunge@gmail.co>
2022-04-12 20:10:18 -0400

    undo incorrect changes

--- clean_run_tests.sh
+++ clean_run_tests.sh
@@ -1,9 +1,9 @@
 RAILS_ENV=test
 PARALLEL_MINITEST=true
 echo "RUNNING IN PARALLEL, TTY IS NOT SUPPORTED! byebug/binding.pry will cause hangs!"
-docker-compose run --rm test rails db:environment:set RAILS_ENV=test
+docker-compose run --rm solutions_test rails db:environment:set RAILS_ENV=test
 echo "Setting: RAILS_ENV=$RAILS_ENV"
-docker-compose run --rm -e PARALLEL_MINITEST test rails db:drop && \
-docker-compose run --rm -e PARALLEL_MINITEST test rails db:create && \
-docker-compose run --rm -e PARALLEL_MINITEST test rails db:migrate && \
-docker-compose run --rm -e PARALLEL_MINITEST test rails test -f --verbose
+docker-compose run --rm -e PARALLEL_MINITEST solutions_test rails db:drop && \
+docker-compose run --rm -e PARALLEL_MINITEST solutions_test rails db:create && \
+docker-compose run --rm -e PARALLEL_MINITEST solutions_test rails db:migrate && \
+docker-compose run --rm -e PARALLEL_MINITEST solutions_test rails test -f --verbose
