9a3295d3eca66086404d8609cb3f180097eae6db
donrestarone <shashikejayatunge@gmail.co>
2022-04-15 01:07:09 -0400

    basic ember demo

--- client/app/router.js
+++ client/app/router.js
@@ -6,4 +6,8 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('api-resources', function () {
+    this.route('resource', { path: '/:version/:slug' });
+  });
+});
