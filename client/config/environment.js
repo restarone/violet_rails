7ba983892f69938c74aee325468af9c022e0a3bc
donrestarone <shashikejayatunge@gmail.co>
2022-04-14 23:35:40 -0400

    fix routing

--- client/config/environment.js
+++ client/config/environment.js
@@ -5,7 +5,6 @@ module.exports = function (environment) {
     modulePrefix: 'client',
     environment,
     rootURL: '/app',
-    baseURL: '/app',
     locationType: 'history',
     EmberENV: {
       FEATURES: {
