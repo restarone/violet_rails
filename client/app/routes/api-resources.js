a6a5737fef9891badbb2d723583d5c1817e5ed4e
donrestarone <shashikejayatunge@gmail.co>
2022-04-15 01:42:58 -0400

    fix routing, now need to fix route model loading in nested route

--- client/app/routes/api-resources.js
+++ client/app/routes/api-resources.js
@@ -1,11 +1,10 @@
 import Route from '@ember/routing/route';
-import fetch from "fetch";
+import fetch from 'fetch';
 
 export default class ApiResourcesRoute extends Route {
   async model() {
     const response = await fetch('/api/resources');
-    let apiResources = await response.json();
-    console.log(apiResources)
-    return apiResources.data;
+    let apiNamespaces = await response.json();
+    return apiNamespaces.data;
   }
 }
