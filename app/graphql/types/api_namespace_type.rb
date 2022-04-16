b12b923c3082196e05711a937c541978229b03b6
donrestarone <shashikejayatunge@gmail.co>
2022-04-16 17:28:47 -0400

    ability to sort, limit and offset publically available API namespaces

--- app/graphql/types/api_namespace_type.rb
+++ app/graphql/types/api_namespace_type.rb
@@ -11,6 +11,27 @@ module Types
     field :namespace_type, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :api_resources, [Types::ApiResourceType], null: false
+
+    # {
+    #   apiResources(limit: 3, orderDirection:"asc", orderDimension: "created_at", offset: 1) {
+    #     id
+    #      properties
+    #   }
+    # }
+    field :api_resources, [Types::ApiResourceType], null: true do
+      description "Returns a list of ApiResources with ordering dimension, order direction, offset and limit"
+      argument :limit, Integer, required: false
+      argument :order_direction, String, required: false
+      argument :order_dimension, String, required: false
+      argument :offset, Integer, required: false
+    end
+
+    def api_resources(args = {})
+      args[:order_dimension] ||= 'created_at'
+      args[:order_direction] ||= 'desc'
+      args[:limit] ||= 50
+      args[:offset] ||= 0
+      ApiResource.all.order("#{args[:order_dimension]} #{args[:order_direction]}").limit(args[:limit]).offset(args[:offset])
+    end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,31 +9,31 @@ module Types
 
     # the field below maps like this. Snake case to camelcase
     # {
-    #   apiNamespaces {
+    #   apiNamespaces(limit: 3, orderDirection:"asc", orderDimension: "created_at", offset: 1) {
     #     id
+    #     slug
+    #     properties
+    #     apiResources {
+    #       id
+    #       properties
+    #     }
     #   }
     # }
-    field :api_namespaces, 
-    [Types::ApiNamespaceType], 
-    null: false,
-    description: "Returns a list of ApiNamespaces"
-
-    # {
-    #   apiResources {
-    #     id
-    #   }
-    # }
-    field :api_resources, 
-    [Types::ApiResourceType], 
-    null: true,
-    description: "Returns a list of ApiResources"
-
-    def api_namespaces
-      ApiNamespace.all
+    
+    field :api_namespaces, [Types::ApiNamespaceType], null: false do
+      description "Returns a list of public ApiNamespaces with ordering dimension, order direction, offset and limit"
+      argument :limit, Integer, required: false
+      argument :order_direction, String, required: false
+      argument :order_dimension, String, required: false
+      argument :offset, Integer, required: false
     end
-
-    def api_resources
-      ApiResource.all
+    
+    def api_namespaces(args = {})
+      args[:order_dimension] ||= 'created_at'
+      args[:order_direction] ||= 'desc'
+      args[:limit] ||= 50
+      args[:offset] ||= 0
+      ApiNamespace.where(requires_authentication: false).order("#{args[:order_dimension]} #{args[:order_direction]}").limit(args[:limit]).offset(args[:offset])
     end
   end
 end
