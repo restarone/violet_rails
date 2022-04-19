ba682dd5cbb9218ca5aa75f300833e6349cdaec7
donrestarone <shashikejayatunge@gmail.com>
2022-04-19 16:34:50 -0400

    uncomitted changes

--- client/app/authenticators/devise.js
+++ client/app/authenticators/devise.js
@@ -1,6 +1,6 @@
 import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
 
 export default class Authenticator extends DeviseAuthenticator {
-  serverTokenEndpoint = '/api/users/sign_in'
-  resourceName = 'user'
+  serverTokenEndpoint = '/api/users/sign_in';
+  resourceName = 'user';
 }
