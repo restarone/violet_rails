e4d49a14552355261fc6535766caa103a537b9fe
Shashike Jayatunge <35935196+donrestarone@users.noreply.github.com>
2022-04-20 18:30:28 -0400

    WIP [API Plugin] Subdomain Events (#431)
    * add basic fixture for plugin_subdomain_events-- PS, you should never write tests that check for object.last unless you know for sure the last object is predictable -- ie; if you created it during the test run
    
    * basic implementation + test to verify that it doesnt work when it shouldnt
    
    * fix tests + add to UI
    
    * book into emailbox
    
    * execute create api actions
    
    * api action payload mapping issue
    
    * fix nesting
    
    * remove byebug
    
    * works in development but this test is failing, need to explore further -- skipping for now
    
    * refactor under test
    
    * fix test
    
    * add trackability to forum + add more tests

---
+++ db/migrate/20220420153229_add_plugin_subdomain_events_to_api.rb
@@ -0,0 +1,34 @@
+class AddPluginSubdomainEventsToApi < ActiveRecord::Migration[6.1]
+  def change
+    # docs: https://github.com/restarone/violet_rails/issues/430
+    ApiNamespace.create!(
+      name: 'subdomain/event',
+      slug: 'subdomain_events',
+      version: 1,
+      requires_authentication: true,
+      namespace_type: 'create-read-update-delete',
+      properties: {
+        model_definition: {
+          record_id: {
+            type: "integer",
+            validations: {
+              allow_blank: false,
+              primary_key: true,
+              unique: true
+            }
+          },
+          record_type: {
+            type: "string",
+            validations: {
+              allow_blank: false
+            }
+          }
+        }
+      }
+    )
+
+    change_table :subdomains do |t|
+      t.boolean :api_plugin_events_enabled, default: false
+    end
+  end
+end
