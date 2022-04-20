229cd1feb5fb6f259210acf358ca58c12a5eed1f
Shashike Jayatunge <35935196+donrestarone@users.noreply.github.com>
2022-04-20 10:52:22 -0400

    dynamic session timeout for users (#427) (#429)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -49,7 +49,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :session_timeoutable_in])
   end
 
   # The path used after sign up.
