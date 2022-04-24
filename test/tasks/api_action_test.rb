9e4476346adc3b47146500f917180b1777612257
Shashike Jayatunge <35935196+donrestarone@users.noreply.github.com>
2022-04-23 23:05:43 -0400

    switch to running in sidekiq (#473)

--- test/tasks/api_action_test.rb
+++ test/tasks/api_action_test.rb
@@ -7,12 +7,14 @@ class ApiActionTest < ActiveSupport::TestCase
     @failed_action = api_actions(:three)
     @failed_action.update(lifecycle_stage: 'failed')
     Rails.application.load_tasks if Rake::Task.tasks.empty?
+    Sidekiq::Testing.fake!
   end
 
   test 'rerun failed api actions' do
     failed_action_counts = @api_namespace.executed_api_actions.where(lifecycle_stage: 'failed').size
     assert_difference "@api_namespace.reload.executed_api_actions.where(lifecycle_stage: 'failed').size", -(failed_action_counts) do
       Rake::Task["api_action:rerun_failed_actions"].invoke
+      Sidekiq::Worker.drain_all
     end
   end
 end
