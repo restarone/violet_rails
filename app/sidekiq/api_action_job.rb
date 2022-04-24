7e7eec6bc80a3c767eefba8cf83ae4473a0cab95
donrestarone <shashikejayatunge@gmail.com>
2022-04-24 10:45:28 -0400

    dont run in sidekiq, undo to previous state

--- app/sidekiq/api_action_job.rb
+++ app/sidekiq/api_action_job.rb
@@ -2,6 +2,7 @@ class ApiActionJob
   include Sidekiq::Job
 
   def perform(api_action_id)
+    # turning off to debug server issue related to job processing
     api_action = ApiAction.find(api_action_id)
     api_action.execute_action
   end
