83a34c34dc7c1fd91f65f93ffee8c69a07aea662
Pralish Kayastha <pralishkayastha@gmail.com>
2022-05-02 07:39:53 +0545

    raise validation error if provied value is not valid json

--- app/models/concerns/jsonb_fields_parsable.rb
+++ app/models/concerns/jsonb_fields_parsable.rb
@@ -1,17 +1,24 @@
-# Parse jsonb fields before saving to db
+# Ensure jsonb fields always store hash 
+# Parse JSON string to hash before saving to db if provided value is string
+# Raise validation error if JSON string is not parsable
+
 module JsonbFieldsParsable
     extend ActiveSupport::Concern
 
     included do
-      before_save :parse_jsonb_fields
+      validate :ensure_enumerable_in_jsonb_fields
     end
 
-    def parse_jsonb_fields
+    def ensure_enumerable_in_jsonb_fields
       jsonb_fields = self.class.columns.select{|cl| cl.type === :jsonb}.map(&:name).map(&:to_sym)
       jsonb_fields.each do |field|
         field_value = self[field]
         unless field_value.is_a?(Enumerable) || field_value.blank?
-          self[field] = JSON.parse(field_value)
+          begin 
+            self[field] = JSON.parse(field_value)
+          rescue JSON::ParserError => e
+            errors.add(field, 'Invalid Json Format')
+          end
         end
       end
     end
