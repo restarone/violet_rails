6e2c8f0c5bd25bc9ee441a16fe3ecb4204d959f8
Prashant Khadka <alish.khadka@gmail.com>
2022-05-03 23:25:39 +0545

    feat(): showing user's visit count in session detail page conditionally

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ end
 Rails.application.routes.draw do
   # analytics dashboard
   get 'dashboard', controller: 'comfy/admin/dashboard'
-  get 'dashboard/visits/:ahoy_visit_id', to: 'comfy/admin/dashboard#visit', as: :dashboard_visits
+  get 'dashboard/sessions/:ahoy_visit_id', to: 'comfy/admin/dashboard#visit', as: :dashboard_visits
 
   resources :signup_wizard
   resources :signin_wizard
