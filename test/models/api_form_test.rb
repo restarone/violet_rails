e0340683c0f01fdff0c6a7d6fb9c1edd70a7fe14
Pralish Kayastha <pralishkayastha@gmail.com>
2022-05-13 20:45:37 +0545

    non renderable form

--- test/models/api_form_test.rb
+++ test/models/api_form_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ApiFormTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "return true if renderable property is nil" do
+    api_form = api_forms(:one)
+    assert api_form.is_field_renderable?('Test')
+  end
+
+  test "return true if renderable property is 1" do
+    api_form = api_forms(:renderable)
+    assert api_form.is_field_renderable?('Test')
+  end
+
+  test "return false if renderable property is 0" do
+    api_form = api_forms(:non_renderable)
+    assert api_form.is_field_renderable?('Test')
+  end
 end
