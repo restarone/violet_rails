82b452a6c3f7738524b7be346a9bec8f6da38205
donrestarone <shashikejayatunge@gmail.com>
2022-05-14 11:10:45 -0400

    add missing test cases

--- test/models/api_resource_test.rb
+++ test/models/api_resource_test.rb
@@ -5,7 +5,35 @@ class ApiResourceTest < ActiveSupport::TestCase
     skip('need to add before going GA with API pipelines v0.2')
   end
 
-  test "when updated - adheres to validations enforced by API form" do
+  test "when updated - adheres to type validations enforced by API form" do
+    skip('need to add before going GA with API pipelines v0.2')
+  end
+
+  test "when updated - adheres to presence validations enforced by API form" do
+    skip('need to add before going GA with API pipelines v0.2')
+  end
+
+  test "when updated - adheres to includes validations enforced by API form" do
+    skip('need to add before going GA with API pipelines v0.2')
+  end
+
+  test "when updated - adheres to regex validations enforced by API form" do
+    skip('need to add before going GA with API pipelines v0.2')
+  end
+
+  test "when initialized - fires contextual API actions" do
+    skip('need to add before going GA with API pipelines v0.2')
+  end
+
+  test "when created - fires contextual API actions" do
+    skip('need to add before going GA with API pipelines v0.2')
+  end
+
+  test "when updated - fires contextual API actions" do
+    skip('need to add before going GA with API pipelines v0.2')
+  end
+
+  test "when destroyed - fires contextual API actions" do
     skip('need to add before going GA with API pipelines v0.2')
   end
 end
