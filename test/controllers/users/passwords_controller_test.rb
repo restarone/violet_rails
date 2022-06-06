82c42ca52192f273e9d9f3bc110f3548573fd7cf
Prashant Khadka <alish.khadka@gmail.com>
2022-06-06 16:39:34 +0545

    feat(): test case added

--- test/controllers/users/passwords_controller_test.rb
+++ test/controllers/users/passwords_controller_test.rb
@@ -5,21 +5,63 @@ class Users::PasswordsControllerTest < ActionDispatch::IntegrationTest
     @root_user = users(:public)
     @root_subdomain = subdomains(:public)
 
-    @restarone_subdomain = Subdomain.find_by(name: 'restarone').name
+    @restarone_subdomain = Subdomain.find_by(name: 'restarone')
     Apartment::Tenant.switch @restarone_subdomain.name do
       @restarone_user = User.find_by(email: 'contact@restarone.com')
     end
   end
 
-  test "should send email for resetting password of the user" do
+  test "should send email for resetting password of the user in apex domain" do
     payload = {
       user: { email: @root_user.email }
     }
 
     Apartment::Tenant.switch(@root_subdomain.name) do
-      post user_password_url(subdomain: Apartment::Tenant.current), params: payload
-      byebug
-      assert_response :redirect
+      assert_difference "Devise::Mailer.deliveries.size", +1 do
+        post user_password_url(subdomain: Apartment::Tenant.current), params: payload
+        assert_response :redirect
+      end
+
+      assert_match "<p><a href=\"http://#{@root_subdomain.name}.", Devise::Mailer.deliveries.last.body.to_s
+    end
+  end
+
+  test "should send email for resetting password of the user in subdomain" do
+    Apartment::Tenant.switch(@restarone_subdomain.name) do
+      payload = {
+        user: { email: @restarone_user.email }
+      }
+
+      assert_difference "Devise::Mailer.deliveries.size", +1 do
+        post user_password_url(subdomain: Apartment::Tenant.current), params: payload
+        assert_response :redirect
+      end
+
+      assert_match "<p><a href=\"http://#{@restarone_subdomain.name}.", Devise::Mailer.deliveries.last.body.to_s
+    end
+  end
+
+  test "should send email for resetting password of the user in apex domain using the current Apartment::Tenant instead of current Subdomain name" do
+    previous_name = @root_subdomain.name
+    @root_subdomain.update!(name: 'root')
+
+    Apartment::Tenant.switch(previous_name) do
+      payload = {
+        user: { email: @root_user.email }
+      }
+
+      assert_difference "Devise::Mailer.deliveries.size", +1 do
+        post user_password_url(subdomain: Apartment::Tenant.current), params: payload
+        assert_response :redirect
+      end
+
+      current_apartment_tenant_name = Apartment::Tenant.current
+      current_subdomain_name = @root_subdomain.name
+
+      # Subdomain in url should be 'public'
+      assert_match "<p><a href=\"http://#{current_apartment_tenant_name}.", Devise::Mailer.deliveries.last.body.to_s
+      # Subdomain in url should not be 'root'
+      assert_no_match "<p><a href=\"http://#{current_subdomain_name}.", Devise::Mailer.deliveries.last.body.to_s
     end
   end
 end
