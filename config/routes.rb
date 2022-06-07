ae307ce95a253b0d07a0d8890edff0dbf941f5d1
Prashant <alish.khadka@gmail.com>
2022-06-08 02:57:56 +0545

    [Violet Rails Core]: Ability to delete custom event and visits (#674)
    * added delete event feature
    
    * feat(): implementation for deleting custom events and their visits
    
    * feat(): added test cases
    
    * chore(): removing unwanted new lines
    
    * chore(): refactoring newly added action names
    
    Co-authored-by: Chumei <gloria.kcm@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get 'dashboard/sessions/:ahoy_visit_id', to: 'comfy/admin/dashboard#visit', as: :dashboard_visits
   get 'dashboard/events/:ahoy_event_type', to: 'comfy/admin/dashboard#events_detail', as: :dashboard_events
   get 'dashboard/events_list', to: 'comfy/admin/dashboard#events_list', as: :dashboard_events_list
+  delete 'dashboard/events/:ahoy_event_type/destroy_event', to: 'comfy/admin/dashboard#destroy_event', as: :dashboard_destroy_event
+  delete 'dashboard/events/:ahoy_event_type/destroy_visits', to: 'comfy/admin/dashboard#destroy_visits', as: :dashboard_destroy_visits
 
   resources :signup_wizard
   resources :signin_wizard
