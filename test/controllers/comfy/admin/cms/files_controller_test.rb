29a7fd8717e6149fb838e475d8178a8131a8a4f8
Prashant Khadka <alish.khadka@gmail.com>
2022-06-08 21:06:35 +0545

    feat(): added test cases for files#create action

--- test/controllers/comfy/admin/cms/files_controller_test.rb
+++ test/controllers/comfy/admin/cms/files_controller_test.rb
@@ -118,4 +118,73 @@ class Comfy::Admin::Cms::FilesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  test 'files#delete: should successfully delete if permissioned' do
+    Apartment::Tenant.switch @restarone_subdomain.name do
+      @restarone_user.update(can_manage_files: true)
+      sign_in(@restarone_user)
+
+      delete comfy_admin_cms_site_file_url(subdomain: @restarone_subdomain.name, site_id: @site, id: @file)
+    end
+
+    assert_response :redirect
+    expected_message = "File deleted"
+    assert_match expected_message, request.flash[:success]
+    assert_redirected_to comfy_admin_cms_site_files_url(subdomain: @restarone_subdomain.name, site_id: @site)
+  end
+
+  test 'files#create: should deny if not permissioned' do
+    Apartment::Tenant.switch @restarone_subdomain.name do
+      file_count            = -> { Comfy::Cms::File.count }
+      attachment_count      = -> { ActiveStorage::Attachment.count }
+
+      payload = {
+        file: {
+          label:       "Updated File",
+          description: "Updated Description",
+          file:        fixture_file_upload("fixture_image.png", "image/jpeg")
+        },
+        source: 'plupload'
+      }
+
+      @restarone_user.update(can_manage_files: false)
+      sign_in(@restarone_user)
+
+      assert_no_difference [file_count, attachment_count] do
+        post comfy_admin_cms_site_files_url(site_id: @site.id, subdomain: @restarone_subdomain.name), params: payload
+        
+        assert_response :redirect
+        expected_message = "You do not have the permission to do that. Only users who can_manage_files are allowed to perform that action."
+        assert_match expected_message, request.flash[:alert]
+      end
+    end
+  end
+
+  test 'files#create: should sucessfully create if permissioned' do
+    Apartment::Tenant.switch @restarone_subdomain.name do
+      file_count            = -> { Comfy::Cms::File.count }
+      attachment_count      = -> { ActiveStorage::Attachment.count }
+
+      payload = {
+        file: {
+          label:       "Test File",
+          description: "Test Description",
+          file:        fixture_file_upload("fixture_image.png", "image/jpeg")
+        },
+        source: 'plupload'
+      }
+
+      @restarone_user.update(can_manage_files: true)
+      sign_in(@restarone_user)
+
+      assert_difference [file_count, attachment_count] do
+        post comfy_admin_cms_site_files_url(site_id: @site.id, subdomain: @restarone_subdomain.name), params: payload
+        
+        assert_response :ok
+        file = Comfy::Cms::File.last
+        assert_equal "Test File", file.label
+        assert_equal "Test Description", file.description
+      end
+    end
+  end
+
 end
