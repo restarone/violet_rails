5791228cfbcc3fa91ce481b7cd7d59efb8fa82f9
Prashant Khadka <alish.khadka@gmail.com>
2022-06-09 19:15:59 +0545

    feat(): added comment for reference

--- app/javascript/packs/website/call_to_actions.js
+++ app/javascript/packs/website/call_to_actions.js
@@ -7,6 +7,7 @@ export default function ctaSuccessHandler() {
 export function ctaSuccessHandlerRecaptchaV3(elemId, token) {
   ctaSuccessHandler();
 
+  // By default recaptcha calls method: setInputWithRecaptchaResponseTokenFor#{sanitize_action(action)} as callback which sets the value of hidden input to the token.
   const element = document.getElementById(elemId);
   element.value = token;
 }
