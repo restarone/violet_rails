24c7074ef8c4b585b27bbb25cc05e797ba2ca241
Prashant <alish.khadka@gmail.com>
2022-06-09 22:00:29 +0545

    [Violet Rails Core]: Recaptcha v3 option in api form (#702)
    * feat(): recaptcha v3 implementation
    
    * feat(): fix recaptcha v3 verification failing
    
    * feat(): make recaptch v2 & v3 mutually exclusive
    
    * feat(): test cases added
    
    * feat(): recaptch v3 action & minimum threshold refactoring
    
    * feat(): hiding recaptcha badge
    
    * feat(): sanitizing action-name for recaptcha and added test cases
    
    * feat(): added comment for reference

--- app/models/api_form.rb
+++ app/models/api_form.rb
@@ -2,6 +2,8 @@ class ApiForm < ApplicationRecord
   include JsonbFieldsParsable
   belongs_to :api_namespace
 
+  before_save :mutually_exclude_recaptcha_type, if: -> { self.show_recaptcha && self.show_recaptcha_v3 }
+
   INPUT_TYPE_MAPPING = {
     free_text: 'text',
     number: 'number',
@@ -13,7 +15,16 @@ class ApiForm < ApplicationRecord
     tel: 'tel'
   }
 
+  # reference: https://developers.google.com/recaptcha/docs/v3#interpreting_the_score
+  RECAPTCHA_V3_MINIMUM_SCORE = 0.5
+
   def is_field_renderable?(field)
     properties.dig(field.to_s, 'renderable').nil? || properties.dig(field.to_s, 'renderable') == '1'
+  end
+
+  private
+
+  def mutually_exclude_recaptcha_type
+    self.show_recaptcha_v3 = false
   end
 end
