e7744bbf933c85c31417f5b6d91d141c6283bdba
Prashant Khadka <alish.khadka@gmail.com>
2022-06-14 16:42:20 +0545

    feat(): test cases added for external_api_clients_controller

--- test/controllers/admin/comfy/external_api_clients_controller_test.rb
+++ test/controllers/admin/comfy/external_api_clients_controller_test.rb
@@ -17,6 +17,43 @@ class Comfy::Admin::ExternalApiClientsControllerTest < ActionDispatch::Integrati
     @external_api_client.update(status: ExternalApiClient::STATUSES[:stopped], enabled: true)
     get start_api_namespace_external_api_client_path(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
     assert_response :redirect
+    assert_not_empty ExternalApiClientJob.jobs
+    Sidekiq::Worker.drain_all
+  end
+
+  test "#start: should not run sidekiq-job if external-api-client is disabled" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:stopped], enabled: false)
+    
+    sign_in(@user)
+    get start_api_namespace_external_api_client_path(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    
+    assert_response :redirect
+    assert_empty ExternalApiClientJob.jobs
+  
+    Sidekiq::Worker.drain_all
+  end
+
+  test "#start: should not run sidekiq-job if external-api-client is in error status" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:error], enabled: true)
+    
+    sign_in(@user)
+    get start_api_namespace_external_api_client_path(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    
+    assert_response :redirect
+    assert_empty ExternalApiClientJob.jobs
+  
+    Sidekiq::Worker.drain_all
+  end
+
+  test "#start: should not run sidekiq-job if external-api-client is in running status" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:running], enabled: true)
+    
+    sign_in(@user)
+    get start_api_namespace_external_api_client_path(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    
+    assert_response :redirect
+    assert_empty ExternalApiClientJob.jobs
+  
     Sidekiq::Worker.drain_all
   end
 
@@ -25,7 +62,7 @@ class Comfy::Admin::ExternalApiClientsControllerTest < ActionDispatch::Integrati
     assert_response :redirect
   end
 
-  test "should not get #index, #new if signed in but not allowed to manage web" do
+  test "should not get #index, if signed in but not allowed to manage api" do
     @user.update(can_manage_api: false)
     sign_in(@user)
     get api_namespace_external_api_clients_path(api_namespace_id: @api_namespace.id)
@@ -38,27 +75,402 @@ class Comfy::Admin::ExternalApiClientsControllerTest < ActionDispatch::Integrati
     assert_response :success
   end
 
-  test "should get new" do
-    skip
+  test "#new: denies if user not signed in" do
+    get new_api_namespace_external_api_client_path(api_namespace_id: @api_namespace.id)
+    expected_message = "You need to sign in or sign up before continuing."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
   end
 
-  test "should create" do
-    skip
+  test "#new: denies if user not permissioned to manage api" do
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    get new_api_namespace_external_api_client_path(api_namespace_id: @api_namespace.id)
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
   end
 
-  test "should show" do
-    skip
+  test "#new: allows if permissioned user is signed in" do
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    get new_api_namespace_external_api_client_path(api_namespace_id: @api_namespace.id)
+
+    assert_response :success
   end
 
-  test "should get edit" do
-    skip
+  test "#create: should allow when permissioned user is signed-in" do
+    payload = {
+      external_api_client: {
+        api_namespace_id: @api_namespace.id,
+        slug: 'create-test-api',
+        label: 'Create Test API',
+        enabled: true,
+        metadata: {
+                    "api_key": "x-api-key-foo",
+                    "bearer_token": "foo"
+                  },
+        model_definition: "class ExternalApiModelExample; def initialize(parameters); # do init stuff; end; def start; return true; end; def log; return true; end; end; # at the end of the file we have to implicitly return the class; ExternalApiModelExample;"
+      }
+    }
+
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    assert_difference "ExternalApiClient.count", +1 do
+      post api_namespace_external_api_clients_url(api_namespace_id: @api_namespace.id), params: payload
+    end
+
+    expected_message = "Api client was successfully created."
+    assert_match expected_message, flash[:notice]
+    assert_response :redirect
   end
 
-  test "should update" do
-    skip
+  test "#create: should deny when user is not signed-in" do
+    payload = {
+      external_api_client: {
+        api_namespace_id: @api_namespace.id,
+        slug: 'create-test-api',
+        label: 'Create Test API',
+        enabled: true,
+        metadata: {
+                    "api_key": "x-api-key-foo",
+                    "bearer_token": "foo"
+                  },
+        model_definition: "class ExternalApiModelExample; def initialize(parameters); # do init stuff; end; def start; return true; end; def log; return true; end; end; # at the end of the file we have to implicitly return the class; ExternalApiModelExample;"
+      }
+    }
+
+    assert_no_difference "ExternalApiClient.count" do
+      post api_namespace_external_api_clients_url(api_namespace_id: @api_namespace.id), params: payload
+    end
+
+    expected_message = "You need to sign in or sign up before continuing."
+    assert_match expected_message, flash[:alert]
+    assert_response :redirect
   end
 
-  test "should destroy" do
-    skip
+  test "#create: should deny when user is not permissioned to manage api" do
+    payload = {
+      external_api_client: {
+        api_namespace_id: @api_namespace.id,
+        slug: 'create-test-api',
+        label: 'Create Test API',
+        enabled: true,
+        metadata: {
+                    "api_key": "x-api-key-foo",
+                    "bearer_token": "foo"
+                  },
+        model_definition: "class ExternalApiModelExample; def initialize(parameters); # do init stuff; end; def start; return true; end; def log; return true; end; end; # at the end of the file we have to implicitly return the class; ExternalApiModelExample;"
+      }
+    }
+
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    assert_no_difference "ExternalApiClient.count" do
+      post api_namespace_external_api_clients_url(api_namespace_id: @api_namespace.id), params: payload
+    end
+
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+    assert_match expected_message, flash[:alert]
+    assert_response :redirect
+  end
+
+  test "#show: denies if user not signed in" do
+    get api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    expected_message = "You need to sign in or sign up before continuing."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+  end
+
+  test "#show: denies if user not permissioned to manage api" do
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    get api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+  end
+
+  test "#show: allows if permissioned user is signed in" do
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    get api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    assert_response :success
+  end
+
+  test "#edit: denies if user not signed in" do
+    get edit_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    expected_message = "You need to sign in or sign up before continuing."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+  end
+
+  test "#edit: denies if user not permissioned to manage api" do
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    get edit_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+  end
+
+  test "#edit: allows if permissioned user is signed in" do
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    get edit_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    assert_response :success
+  end
+
+  test "#update: should allow when permissioned user is signed-in" do
+    payload = {
+      external_api_client: {
+        api_namespace_id: @api_namespace.id,
+        slug: 'create-test-api',
+        label: 'Create Test API',
+        enabled: true,
+        metadata: {
+                    "api_key": "x-api-key-foo",
+                    "bearer_token": "foo"
+                  },
+        model_definition: "class ExternalApiModelExample; def initialize(parameters); # do init stuff; end; def start; return true; end; def log; return true; end; end; # at the end of the file we have to implicitly return the class; ExternalApiModelExample;"
+      }
+    }
+
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    patch api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id), params: payload
+
+    expected_message = "Api client was successfully updated."
+    assert_match expected_message, flash[:notice]
+    assert_response :redirect
+  end
+
+  test "#update: should deny when user is not signed-in" do
+    payload = {
+      external_api_client: {
+        api_namespace_id: @api_namespace.id,
+        slug: 'create-test-api',
+        label: 'Create Test API',
+        enabled: true,
+        metadata: {
+                    "api_key": "x-api-key-foo",
+                    "bearer_token": "foo"
+                  },
+        model_definition: "class ExternalApiModelExample; def initialize(parameters); # do init stuff; end; def start; return true; end; def log; return true; end; end; # at the end of the file we have to implicitly return the class; ExternalApiModelExample;"
+      }
+    }
+
+    patch api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id), params: payload
+
+    expected_message = "You need to sign in or sign up before continuing."
+    assert_match expected_message, flash[:alert]
+    assert_response :redirect
+  end
+
+  test "#update: should deny when user is not permissioned to manage api" do
+    payload = {
+      external_api_client: {
+        api_namespace_id: @api_namespace.id,
+        slug: 'create-test-api',
+        label: 'Create Test API',
+        enabled: true,
+        metadata: {
+                    "api_key": "x-api-key-foo",
+                    "bearer_token": "foo"
+                  },
+        model_definition: "class ExternalApiModelExample; def initialize(parameters); # do init stuff; end; def start; return true; end; def log; return true; end; end; # at the end of the file we have to implicitly return the class; ExternalApiModelExample;"
+      }
+    }
+
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    patch api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id), params: payload
+
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+    assert_match expected_message, flash[:alert]
+    assert_response :redirect
+  end
+
+  test "#destroy: denies if user not signed in" do
+    assert_no_difference "ExternalApiClient.count" do
+      delete api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    end
+
+    expected_message = "You need to sign in or sign up before continuing."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+  end
+
+  test "#destroy: denies if user not permissioned to manage api" do
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    assert_no_difference "ExternalApiClient.count" do
+      delete api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    end
+
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+  end
+
+  test "#destroy: allows if permissioned user is signed in" do
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    assert_difference "ExternalApiClient.count", -1 do
+      delete api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+    end
+
+    expected_message = "Api client was successfully destroyed."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:notice]
+  end
+
+  test "#stop: denies if user not signed in" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:running])
+
+    get stop_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    expected_message = "You need to sign in or sign up before continuing."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+    assert_not_equal ExternalApiClient::STATUSES[:stopped], @external_api_client.reload.status
+  end
+
+  test "#stop: denies if user not permissioned to manage api" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:running])
+
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    get stop_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+    assert_not_equal ExternalApiClient::STATUSES[:stopped], @external_api_client.reload.status
+  end
+
+  test "#stop: allows if permissioned user is signed in" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:running])
+
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    get stop_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    assert_response :redirect
+    assert_equal ExternalApiClient::STATUSES[:stopped], @external_api_client.reload.status
+  end
+
+  test "#clear_errors: denies if user not signed in" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:error], error_message: 'test error message', retries: 3, error_metadata: { 'testKey': 'testMessage'})
+
+    get clear_errors_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    expected_message = "You need to sign in or sign up before continuing."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+    assert_not_equal ExternalApiClient::STATUSES[:stopped], @external_api_client.reload.status
+    assert_not_equal 0, @external_api_client.reload.retries
+    assert @external_api_client.reload.error_message
+    assert @external_api_client.reload.error_metadata
+  end
+
+  test "#clear_errors: denies if user not permissioned to manage api" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:error], error_message: 'test error message', retries: 3, error_metadata: { 'testKey': 'testMessage'})
+
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    get clear_errors_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+    assert_not_equal ExternalApiClient::STATUSES[:stopped], @external_api_client.reload.status
+    assert_not_equal 0, @external_api_client.reload.retries
+    assert @external_api_client.reload.error_message
+    assert @external_api_client.reload.error_metadata
+  end
+
+  test "#clear_errors: allows if permissioned user is signed in" do
+    @external_api_client.update(status: ExternalApiClient::STATUSES[:error], error_message: 'test error message', retries: 3, error_metadata: { 'testKey': 'testMessage'})
+
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    get clear_errors_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    assert_response :redirect
+    assert_equal ExternalApiClient::STATUSES[:stopped], @external_api_client.reload.status
+    assert_equal 0, @external_api_client.reload.retries
+    refute @external_api_client.reload.error_message
+    refute @external_api_client.reload.error_metadata
+  end
+
+  test "#clear_state: denies if user not signed in" do
+    @external_api_client.update(state_metadata: { 'testKey': 'testMessage'})
+
+    get clear_state_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    expected_message = "You need to sign in or sign up before continuing."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+    assert @external_api_client.reload.state_metadata
+  end
+
+  test "#clear_state: denies if user not permissioned to manage api" do
+    @external_api_client.update(state_metadata: { 'testKey': 'testMessage'})
+
+    @user.update(can_manage_api: false)
+    sign_in(@user)
+
+    get clear_state_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    expected_message = "You do not have the permission to do that. Only users who can_manage_api are allowed to perform that action."
+
+    assert_response :redirect
+    assert_match expected_message, flash[:alert]
+    assert @external_api_client.reload.state_metadata
+  end
+
+  test "#clear_state: allows if permissioned user is signed in" do
+    @external_api_client.update(state_metadata: { 'testKey': 'testMessage'})
+
+    @user.update(can_manage_api: true)
+    sign_in(@user)
+
+    get clear_state_api_namespace_external_api_client_url(api_namespace_id: @api_namespace.id, id: @external_api_client.id)
+
+    assert_response :redirect
+    refute @external_api_client.reload.state_metadata
   end
 end
