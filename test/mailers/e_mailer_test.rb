f1372aa566572af210f2cce37c439923981a49c0
Shashike Jayatunge <35935196+donrestarone@users.noreply.github.com>
2022-06-16 17:10:04 -0400

    [feature] allow multiple recipients in email (#768)
    resolves #149 
    
    - connected select2 to recipients field
    - previous `EMailer.ship` attempted to send separate email for each recipient, only the last recipient was being sent to
    - EMailer now correctly fills the recipients array
    
    Co-authored-by: Yuriy Yashkir <yuriy.yashkir@gmail.com>

--- test/mailers/e_mailer_test.rb
+++ test/mailers/e_mailer_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class EMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'sends email to multiple recipients' do
+    recipients = ['a@a.com', 'b@b.com', 'c@c.com']
+
+    message_thread = MessageThread.new(recipients: recipients)
+    message = Message.new(content: 'content')
+    email = EMailer.with(message: message, message_thread: message_thread).ship
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal recipients, email.to
+  end
 end
