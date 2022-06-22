c68f7d7142e2c8800f69dec6f53900127cbf3c5d
Prashant <alish.khadka@gmail.com>
2022-06-22 06:22:07 +0545

    [Violet Rails Core] add pagination + search to email threads/index (#788)
    * feat(): implementation for pagination and searching by email-body, subject or sender-email adrress in mailbox
    
    * feat(): added test cases

--- test/controllers/mailbox/mailbox_controller_test.rb
+++ test/controllers/mailbox/mailbox_controller_test.rb
@@ -43,4 +43,109 @@ class Mailbox::MailboxControllerTest < ActionDispatch::IntegrationTest
     get mailbox_url
     assert_response :success
   end
+
+  test "#show: searches email-threads by email-body content" do
+    other_user = users(:one)
+
+    message_thread_1 = MessageThread.first
+    message_1 = message_thread_1.messages.create!(content: '<div>body 1</div>')
+
+    message_thread_2 = MessageThread.create!(unread: true, subject: 'test', recipients: [other_user.email])
+    message_2 = message_thread_2.messages.create!(content: '<div>body 2</div>')
+
+    message_thread_3 = MessageThread.create!(unread: true, subject: 'new email', recipients: [other_user.email])
+    message_3 = message_thread_3.messages.create!(content: '<div>description</div>')
+
+    message_thread_4 = MessageThread.create!(unread: true, subject: 'restarone', recipients: [other_user.email])
+    message_4 = message_thread_4.messages.create!(content: '<div>violet</div>')
+
+    payload = {
+      q: {
+        messages_content_body_or_subject_or_messages_from_cont: 'body'
+      }
+    }
+    
+    sign_in(@user)
+    get mailbox_url, params: payload
+
+    assert_response :success
+    
+    message_threads = @controller.view_assigns['message_threads']
+
+    assert_equal [message_thread_1.id, message_thread_2.id].sort, message_threads.pluck(:id).sort
+    
+    message_threads.each do |message_thread|
+      assert_match 'body', message_thread.messages.first.content.body.to_s
+    end
+  end
+
+  test "#show: searches email-threads by email-subject" do
+    other_user = users(:one)
+
+    message_thread_1 = MessageThread.first
+    message_1 = message_thread_1.messages.create!(content: '<div>body 1</div>')
+
+    message_thread_2 = MessageThread.create!(unread: true, subject: 'test', recipients: [other_user.email])
+    message_2 = message_thread_2.messages.create!(content: '<div>body 2</div>')
+
+    message_thread_3 = MessageThread.create!(unread: true, subject: 'new email', recipients: [other_user.email])
+    message_3 = message_thread_3.messages.create!(content: '<div>description</div>')
+
+    message_thread_4 = MessageThread.create!(unread: true, subject: 'restarone', recipients: [other_user.email])
+    message_4 = message_thread_4.messages.create!(content: '<div>violet</div>')
+
+    payload = {
+      q: {
+        messages_content_body_or_subject_or_messages_from_cont: 'restarone'
+      }
+    }
+    
+    sign_in(@user)
+    get mailbox_url, params: payload
+
+    assert_response :success
+    
+    message_threads = @controller.view_assigns['message_threads']
+
+    assert_equal [message_thread_4.id].sort, message_threads.pluck(:id).sort
+    
+    message_threads.each do |message_thread|
+      assert_match 'restarone', message_thread.subject
+    end
+  end
+
+  test "#show: searches email-threads by sender-email-address" do
+    other_user = users(:one)
+
+    message_thread_1 = MessageThread.first
+    message_1 = message_thread_1.messages.create!(content: '<div>body 1</div>')
+
+    message_thread_2 = MessageThread.create!(unread: true, subject: 'test', recipients: [other_user.email])
+    message_2 = message_thread_2.messages.create!(content: '<div>body 2</div>')
+
+    message_thread_3 = MessageThread.create!(unread: true, subject: 'new email', recipients: [other_user.email])
+    message_3 = message_thread_3.messages.create!(content: '<div>description</div>', from: 'violet@rails.com')
+
+    message_thread_4 = MessageThread.create!(unread: true, subject: 'restarone', recipients: [other_user.email])
+    message_4 = message_thread_4.messages.create!(content: '<div>violet</div>', from: 'violet@rails.com')
+
+    payload = {
+      q: {
+        messages_content_body_or_subject_or_messages_from_cont: 'violet@rails.com'
+      }
+    }
+    
+    sign_in(@user)
+    get mailbox_url, params: payload
+
+    assert_response :success
+    
+    message_threads = @controller.view_assigns['message_threads']
+
+    assert_equal [message_thread_3.id, message_thread_4.id].sort, message_threads.pluck(:id).sort
+    
+    message_threads.each do |message_thread|
+      assert_match 'violet@rails.com', message_thread.messages.first.from
+    end
+  end
 end
