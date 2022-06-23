ebff37d66124639529b586974c133f633a046871
Prashant <alish.khadka@gmail.com>
2022-06-23 16:29:57 +0545

    feat(): add functionality to search my recipients email (#794)

--- test/controllers/mailbox/mailbox_controller_test.rb
+++ test/controllers/mailbox/mailbox_controller_test.rb
@@ -61,7 +61,7 @@ class Mailbox::MailboxControllerTest < ActionDispatch::IntegrationTest
 
     payload = {
       q: {
-        messages_content_body_or_subject_or_messages_from_cont: 'body'
+        search_messages_content_body_or_subject_or_messages_from_or_recipients_contains: 'body'
       }
     }
     
@@ -91,12 +91,12 @@ class Mailbox::MailboxControllerTest < ActionDispatch::IntegrationTest
     message_thread_3 = MessageThread.create!(unread: true, subject: 'new email', recipients: [other_user.email])
     message_3 = message_thread_3.messages.create!(content: '<div>description</div>')
 
-    message_thread_4 = MessageThread.create!(unread: true, subject: 'restarone', recipients: [other_user.email])
+    message_thread_4 = MessageThread.create!(unread: true, subject: 'restarone inc', recipients: [other_user.email])
     message_4 = message_thread_4.messages.create!(content: '<div>violet</div>')
 
     payload = {
       q: {
-        messages_content_body_or_subject_or_messages_from_cont: 'restarone'
+        search_messages_content_body_or_subject_or_messages_from_or_recipients_contains: 'restarone inc'
       }
     }
     
@@ -131,7 +131,7 @@ class Mailbox::MailboxControllerTest < ActionDispatch::IntegrationTest
 
     payload = {
       q: {
-        messages_content_body_or_subject_or_messages_from_cont: 'violet@rails.com'
+        search_messages_content_body_or_subject_or_messages_from_or_recipients_contains: 'violet@rails.com'
       }
     }
     
@@ -146,6 +146,41 @@ class Mailbox::MailboxControllerTest < ActionDispatch::IntegrationTest
     
     message_threads.each do |message_thread|
       assert_match 'violet@rails.com', message_thread.messages.first.from
+    end
+  end
+
+  test "#show: searches email-threads by recipients-email-address" do
+    other_user = users(:one)
+
+    message_thread_1 = MessageThread.first
+    message_1 = message_thread_1.messages.create!(content: '<div>body 1</div>')
+
+    message_thread_2 = MessageThread.create!(unread: true, subject: 'test', recipients: [other_user.email])
+    message_2 = message_thread_2.messages.create!(content: '<div>body 2</div>')
+
+    message_thread_3 = MessageThread.create!(unread: true, subject: 'new email', recipients: [other_user.email])
+    message_3 = message_thread_3.messages.create!(content: '<div>description</div>', from: 'violet@rails.com')
+
+    message_thread_4 = MessageThread.create!(unread: true, subject: 'restarone', recipients: [other_user.email])
+    message_4 = message_thread_4.messages.create!(content: '<div>violet</div>', from: 'violet@rails.com')
+
+    payload = {
+      q: {
+        search_messages_content_body_or_subject_or_messages_from_or_recipients_contains: other_user.email
+      }
+    }
+    
+    sign_in(@user)
+    get mailbox_url, params: payload
+
+    assert_response :success
+    
+    message_threads = @controller.view_assigns['message_threads']
+
+    assert_equal [message_thread_2.id, message_thread_3.id, message_thread_4.id].sort, message_threads.pluck(:id).sort
+    
+    message_threads.each do |message_thread|
+      assert_match other_user.email, message_thread.recipients.to_s
     end
   end
 end
