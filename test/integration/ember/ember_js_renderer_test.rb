201bf28b875ab58bdc527e029a6b863cec1b6e0b
donrestarone <shashikejayatunge@gmail.co>
2022-06-23 19:23:42 -0400

    update deprecation message

--- test/integration/ember/ember_js_renderer_test.rb
+++ test/integration/ember/ember_js_renderer_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class EmberJsRendererTest < ActionDispatch::IntegrationTest
   test "should render ember app if enabled" do
-    skip('ember is broken in ruby 3 due to an issue in ember-cli-rails')
+    skip('ember is broken in ruby 3 due to an issue in ember-cli-rails and is soft deprecated and scheduled for removal in Violet Rails 1.0 (see: https://github.com/restarone/violet_rails/pull/371)')
     subdomains(:public).update!(ember_enabled: true)
     get '/app'
     assert_response :success
@@ -10,7 +10,7 @@ class EmberJsRendererTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect if not enabled" do
-    skip('ember is broken in ruby 3 due to an issue in ember-cli-rails')
+    skip('ember is broken in ruby 3 due to an issue in ember-cli-rails and is soft deprecated and scheduled for removal in Violet Rails 1.0 (see: https://github.com/restarone/violet_rails/pull/371)')
     subdomains(:public).update!(ember_enabled: false)
     get '/app'
     follow_redirect!
