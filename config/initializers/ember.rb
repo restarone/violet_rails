f3ed39413ec01cb9e694eaabd5830f37cb9480e7
Shashike Jayatunge <35935196+donrestarone@users.noreply.github.com>
2022-06-23 20:43:07 -0400

    prevent ember from loading if its ruby 3 (#800)
    Co-authored-by: donrestarone <shashikejayatunge@gmail.co>

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -1,3 +1,5 @@
-# EmberCli.configure do |c|
-#   c.app :client
-# end
+if RUBY_VERSION != '3.0.0'
+  EmberCli.configure do |c|
+    c.app :client
+  end
+end
--- test/integration/ember/ember_js_renderer_test.rb
+++ test/integration/ember/ember_js_renderer_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 
 class EmberJsRendererTest < ActionDispatch::IntegrationTest
   test "should render ember app if enabled" do
-    skip('ember is broken in ruby 3 due to an issue in ember-cli-rails and is soft deprecated and scheduled for removal in Violet Rails 1.0 (see: https://github.com/restarone/violet_rails/pull/371)')
+    if RUBY_VERSION == "3.0.0"
+      skip('ember is broken in ruby 3 due to an issue in ember-cli-rails and is soft deprecated and scheduled for removal in Violet Rails 1.0 (see: https://github.com/restarone/violet_rails/pull/371)')
+    end
     subdomains(:public).update!(ember_enabled: true)
     get '/app'
     assert_response :success
@@ -10,7 +12,9 @@ class EmberJsRendererTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect if not enabled" do
-    skip('ember is broken in ruby 3 due to an issue in ember-cli-rails and is soft deprecated and scheduled for removal in Violet Rails 1.0 (see: https://github.com/restarone/violet_rails/pull/371)')
+    if RUBY_VERSION == "3.0.0"
+      skip('ember is broken in ruby 3 due to an issue in ember-cli-rails and is soft deprecated and scheduled for removal in Violet Rails 1.0 (see: https://github.com/restarone/violet_rails/pull/371)')
+    end
     subdomains(:public).update!(ember_enabled: false)
     get '/app'
     follow_redirect!
