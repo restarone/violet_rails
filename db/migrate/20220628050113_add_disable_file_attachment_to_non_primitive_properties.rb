8436ac0fb28345c8e45e983217ec6df2fe216457
Prashant Khadka <prashant.khadka052@gmail.com>
2022-06-28 21:51:51 +0545

    feat(): rename disable_file_attachment to allow_attachments column

--- db/migrate/20220628050113_add_disable_file_attachment_to_non_primitive_properties.rb
+++
@@ -1,5 +0,0 @@
-class AddDisableFileAttachmentToNonPrimitiveProperties < ActiveRecord::Migration[6.1]
-  def change
-    add_column :non_primitive_properties, :disable_file_attachment, :boolean, default: false
-  end
-end
