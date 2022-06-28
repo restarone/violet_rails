605c2198c666683a17cbbf7087cc44b0ed853caf
Prashant Khadka <prashant.khadka052@gmail.com>
2022-06-28 15:48:31 +0545

    feat(): add before_save hook for resetting disable_file_attachment value for non-richtext type

--- app/models/non_primitive_property.rb
+++ app/models/non_primitive_property.rb
@@ -8,4 +8,12 @@ class NonPrimitiveProperty < ApplicationRecord
   has_one_attached :attachment
 
   validates_presence_of :label
+
+  before_save :reset_disable_file_attachment
+
+  private
+
+  def reset_disable_file_attachment
+    self.disable_file_attachment = false if self.field_type != 'richtext' && self.disable_file_attachment
+  end
 end
