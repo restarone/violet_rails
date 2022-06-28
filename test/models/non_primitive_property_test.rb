231dbd815dcdfac09efd1b86f92857a79675ef83
Prashant Khadka <prashant.khadka052@gmail.com>
2022-06-28 19:27:56 +0545

    feat(): added test cases for non-primitive-property model

--- test/models/non_primitive_property_test.rb
+++ test/models/non_primitive_property_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class NonPrimitivePropertyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should reset disable_file_attachment if field_type is not richtext" do
+    non_primitive_property = non_primitive_properties(:one)
+    non_primitive_property.update!(field_type: 'file')
+    
+    non_primitive_property.update!(disable_file_attachment: true)
+    refute non_primitive_property.reload.disable_file_attachment
+  end
+
+  test "should not reset disable_file_attachment if field_type is richtext" do
+    non_primitive_property = non_primitive_properties(:one)
+    non_primitive_property.update!(field_type: 'richtext')
+    
+    expected_value = true
+    non_primitive_property.update!(disable_file_attachment: expected_value)
+    assert_equal expected_value, non_primitive_property.reload.disable_file_attachment
+  end
 end
