bdf3bb00bb5888e2342a54816adf02f3491262d4
Prashant Khadka <prashant.khadka052@gmail.com>
2022-06-30 10:54:07 +0545

    fix(): show error messages while invalid login attempt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+class RSolutionsFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
+    end
+  end
+
+end
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -326,10 +337,11 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
+  config.warden do |manager|
+    manager.failure_app = RSolutionsFailureApp
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
-  # end
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
