adbe73ea8ff7fee024dc2039ec0b240210494ca6
Pralish Kayastha <50227291+Pralish@users.noreply.github.com>
2022-07-04 02:53:46 +0545

    [feature] Dynamic attributes in API Actions (#784)
    addresses: https://github.com/restarone/violet_rails/issues/653
    
    
    addresses: https://github.com/restarone/violet_rails/issues/652
    
    upgrade path: https://github.com/restarone/violet_rails/issues/836
    
    
    # Feature Showcase
    
    ## Dynamic Emails
    Given a simple form like this: 
    ![Screenshot from 2022-07-03 14-27-25](https://user-images.githubusercontent.com/35935196/177052592-099d22f2-5df7-4f19-b8f7-3178275d3a82.png)
    
    
    
    an email like this is triggered: 
    ![Screenshot from 2022-07-03 14-24-27](https://user-images.githubusercontent.com/35935196/177052558-041538fb-82f3-4caf-b9f0-05b1463c6810.png)
    
    the system will generate the email message: 
    ![Screenshot from 2022-07-03 14-25-07](https://user-images.githubusercontent.com/35935196/177052571-614f4cd4-1596-4d44-a16c-fd63a68caef1.png)

--- app/mailers/api_action_mailer.rb
+++ app/mailers/api_action_mailer.rb
@@ -1,14 +1,14 @@
 class ApiActionMailer < ApplicationMailer
     helper ApiResourcesHelper
     def send_email(api_action)
-      mail_to = api_action.email
+      mail_to = api_action.email_evaluated
       return if mail_to.empty?
     
       @api_action = api_action
       @api_resource = api_action.api_resource
       mail(
         to: mail_to,
-        subject: "#{api_action.type} #{@api_resource.api_namespace.name.pluralize} v#{@api_resource.api_namespace.version}"
+        subject: api_action.email_subject_evaluated || "#{api_action.type} #{@api_resource.api_namespace.name.pluralize} v#{@api_resource.api_namespace.version}"
       )
     end
 end
