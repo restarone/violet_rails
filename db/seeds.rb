d6ae74e8391e7a5569ef58d0a006ed1890a3da51
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2022-07-09 23:05:23 -0400

    seeds: add Faker seed data and remove subdomain

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,90 +1,75 @@
+require 'faker'
 require_relative './violet_seeds/violet.rb'
 
-# This user gets created for all tenants
-user = User.create!(
+# set a deterministic seed
+Faker::Config.random = Random.new(123)
+
+# admin
+user_violet = User.create!(
   email: 'violet@rails.com', 
+  name: 'Violet Rails Admin',
   password: '123456', 
   password_confirmation: '123456', 
   global_admin: true, 
   confirmed_at: Time.now
 )
-user.update!(User::FULL_PERMISSIONS)
+user_violet.update!(User::FULL_PERMISSIONS)
 
-# seeds.rb gets run for each tenant, so we must make sure to run
-# Subdomain.create only once here.
-if Apartment::Tenant.current == 'public'
-  Subdomain.unsafe_bootstrap_www_subdomain
-  Subdomain.create!(name: 'violet')
+Subdomain.unsafe_bootstrap_www_subdomain
+
+# other users
+users = []
+5.times do
+  new_user = User.create!(
+    email: Faker::Internet.unique.email, 
+    name: Faker::Name.name,
+    password: '123456', 
+    password_confirmation: '123456', 
+    global_admin: false, 
+    confirmed_at: Time.now
+  )
+  new_user.update!(User::FULL_PERMISSIONS)
+  users.push new_user
 end
 
-if Apartment::Tenant.current == 'violet'
-  user_orange = User.create!(
-    email: 'orange@rails.com', 
-    name: 'Orange Tester',
-    password: '123456', 
-    password_confirmation: '123456', 
-    global_admin: false, 
-    confirmed_at: Time.now
-  )
-  user_orange.update!(User::FULL_PERMISSIONS)
-
-  user_red = User.create!(
-    email: 'red@rails.com', 
-    name: 'Red Tester',
-    password: '123456', 
-    password_confirmation: '123456', 
-    global_admin: false, 
-    confirmed_at: Time.now
-  )
-  user_red.update!(User::FULL_PERMISSIONS)
-
-  user_blue = User.create!(
-    email: 'blue@rails.com', 
-    name: 'Blue Tester',
-    password: '123456', 
-    password_confirmation: '123456', 
-    global_admin: false, 
-    confirmed_at: Time.now
-  )
-  user_blue.update!(User::FULL_PERMISSIONS)
-
-  # Site
-  # ----
-  site = Comfy::Cms::Site.first
-  site.layouts.find_by(identifier: 'default').update(
-    content: "
+# Site
+# ----
+site = Comfy::Cms::Site.first
+site.layouts.find_by(identifier: 'default').update(
+  content: "
 {{cms:snippet navbar}}
 {{cms:wysiwyg content}}
 {{cms:snippet footer}}
-    ",
-    css: VioletSeeds::SITE_CSS
-    )
-
-  # Pages
-  # -----
-  page = site.pages.first
-  page.update(label: 'landing page')
-  page.fragments.first.update(
-    content: VioletSeeds::LANDING_PAGE_CONTENT
-    )
-
-  # Snippets
-  # --------
-  site.snippets.create(
-    label: 'navbar', 
-    identifier: 'navbar',
-    content: VioletSeeds::NAVBAR_CONTENT
+  ",
+  css: VioletSeeds::SITE_CSS
   )
-  site.snippets.create(
-    label: 'footer', 
-    identifier: 'footer',
-    content: VioletSeeds::FOOTER_CONTENT
+
+# Pages
+# -----
+page = site.pages.first
+page.update(label: 'landing page')
+page.fragments.first.update(
+  content: VioletSeeds::LANDING_PAGE_CONTENT
   )
-    
-  # Blog posts
-  # ----------
+
+# Snippets
+# --------
+site.snippets.create(
+  label: 'navbar', 
+  identifier: 'navbar',
+  content: VioletSeeds::NAVBAR_CONTENT
+)
+site.snippets.create(
+  label: 'footer', 
+  identifier: 'footer',
+  content: VioletSeeds::FOOTER_CONTENT
+)
+  
+# Blog posts
+# ----------
+5.times do
   blog_post = site.blog_posts.create!(
-    title: "My first post",
+    title: Faker::Marketing.unique.buzzwords,
     layout: site.layouts.find_by(identifier: 'default')
   )
   Comfy::Cms::Fragment.create!(
@@ -92,46 +77,55 @@ if Apartment::Tenant.current == 'violet'
     record: blog_post,
     tag: 'wysiwyg',
     content: "
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quae duo sunt, unum facit. Uterque enim summo bono fruitur,
-      id est voluptate. Magna laus. Eam stabilem appellas. Duo Reges: constructio interrete. Negat esse eam, inquit, propter
-      se expetendam. Hunc vos beatum; Qualem igitur hominem natura inchoavit? </p>
-
-      <p>Laboro autem non sine causa; Memini vero, inquam; Bonum incolumis acies: misera caecitas. </p>
-
-      <p>In schola desinis. De quibus cupio scire quid sentias. </p>
+      <p>#{Faker::GreekPhilosophers.quote}</p>
+      <p>#{Faker::GreekPhilosophers.quote}</p>
+      <p>#{Faker::GreekPhilosophers.quote}</p>
     "
   )
+end
 
-  # Forum
-  # -----
-  ForumCategory.create!(name: "cats", slug: "cats2", color: "#0000CC")
-  ForumCategory.create!(name: "dogs", slug: "dogs2", color: "#00CCCC")
+# Forum
+# -----
+ForumCategory.create!(name: Faker::Educator.subject, slug: "cat1", color: "#0000CC")
+ForumCategory.create!(name: Faker::Educator.subject, slug: "cat2", color: "#00CCCC")
 
-  thread = user.forum_threads.create!(title: "Cats are great?", forum_category: ForumCategory.first)
-  post = user.forum_posts.create!(forum_thread: thread, body: "I Think they are awesome!")
-  post = user_orange.forum_posts.create!(forum_thread: thread, body: "I Think they are not so good!")
-  # TODO attachments
+5.times do
+  thread = users.first.forum_threads.create!(title: Faker::Educator.course_name, forum_category: ForumCategory.first)
+  post = users.first.forum_posts.create!(forum_thread: thread, body: Faker::GreekPhilosophers.quote)
+  post = users.second.forum_posts.create!(forum_thread: thread, body: Faker::GreekPhilosophers.quote)
+  post = users.first.forum_posts.create!(forum_thread: thread, body: Faker::GreekPhilosophers.quote)
+  post = users.third.forum_posts.create!(forum_thread: thread, body: Faker::GreekPhilosophers.quote)
+end
 
-  # Emails
-  # ------
-  email_thread = MessageThread.create!(recipients: ['violet@rails.com', 'red@rails.com'] )
-  email_thread.messages.create(content: "An invitation to a meeting. Please come here at 4pm on Friday.")
-  email_thread.messages.create(content: "I will be there", from: 'violet@rails.com')
-  email_thread.messages.create(content: "Have to skip this one.", from: 'red@rails.com')
+# Emails
+# ------
+3.times do
+  recipients = [Faker::Internet.email, Faker::Internet.email] 
+  email_thread = MessageThread.create!(recipients: recipients)
+  3.times do
+    email_thread.messages.create(content: "
+                                 <h5>#{Faker::Movie.quote}</h5>
+                                 <p>#{Faker::Movie.quote} <b>#{Faker::Movie.quote}</b></p>
+                                 ")
+    email_thread.messages.create(content: "<p>#{Faker::Movie.quote} <b>#{Faker::Movie.quote}</b> #{Faker::Movie.quote}</p>",
+                                 from: recipients.first)
+  end
+end
 
-  email_thread = MessageThread.create!(recipients: ['violet@rails.com'] )
-  email_thread.messages.create(content: "I really like your site!", from: 'violet@rails.com')
+email_thread = MessageThread.create!(recipients: ['violet@rails.com'] )
+email_thread.messages.create(content: "I really like your site!", from: 'violet@rails.com')
 
-  # Analytics
-  # ---------
+# Analytics
+# ---------
+10.times do
   visit = Ahoy::Visit.create!(
     started_at: Time.now,
-    ip: '200.200.200.200', 
+    ip: Faker::Internet.ip_v4_address, 
     os: 'GNU/Linux', 
     browser: 'Firefox', 
     device_type: 'Desktop', 
-    user_agent: 'Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0', 
-    landing_page: 'http://violet.localhost:5250/'
+    user_agent: Faker::Internet.user_agent, 
+    landing_page: 'http://localhost:5250/'
   )
   visit.events.create(name: 'button-click', properties: { target: 'checkbox-one' }, time: Time.now - 5.minutes)
   visit.events.create(name: 'button-click', properties: { target: 'checkbox-two' }, time: Time.now - 4.minutes)
