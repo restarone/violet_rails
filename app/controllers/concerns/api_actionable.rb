020e87753b7975337b0fd19f7d1b24747c3036fd
Shashike Jayatunge <35935196+donrestarone@users.noreply.github.com>
2022-07-12 17:41:53 -0400

    [feature] Allow dynamic segment in api redirect action
    # Allow dynamic segment in api redirect action
    
    This feature allows Ruby evaluation in the redirect action so you can do conditional redirects and other fanciness: 
    
    <img width="1728" alt="Screen Shot 2022-07-08 at 11 14 21 AM" src="https://user-images.githubusercontent.com/35935196/178021412-90005e11-a8bb-48d1-b260-3f63320eeaac.png">
    
    ---
    ## How it works
    
    The implicit return value of the evaluated statement should be a valid path in the application. As illustrated above, you can use an absolute path (like: `'/'`, or a Rails route helper like `dashboard_path`)
    
    ---
    
    Addresses: https://github.com/restarone/violet_rails/issues/873
    
    
    ## Video demo
    
    
    https://user-images.githubusercontent.com/35935196/178011977-82aab515-87b5-49a3-8591-f8ba3535d9eb.mp4
    
    ## The evaluated output of the dynamic_url segment  is saved in the lifecycle message
    ![image](https://user-images.githubusercontent.com/25191509/178540033-bba9e309-b9bb-45b6-a438-b8cfd5716663.png)
    
    
    Co-authored-by: Prashant Khadka <prashant.khadka052@gmail.com>

--- app/controllers/concerns/api_actionable.rb
+++ app/controllers/concerns/api_actionable.rb
@@ -10,11 +10,19 @@ module ApiActionable
 
 
   def check_for_redirect_action
-    @redirect_action = @api_namespace.send(api_action_name).where(action_type: 'redirect').last
+    @redirect_action = if @api_resource.present?
+                        @api_resource.send(api_action_name).where(action_type: 'redirect').reorder(:created_at).last
+                      else
+                        @api_namespace.send(api_action_name).where(action_type: 'redirect').last
+                      end
   end
 
   def check_for_serve_file_action
-    serve_file_action = @api_namespace.send(api_action_name).where(action_type: 'serve_file').last
+    serve_file_action = if @api_resource.present?
+                        @api_resource.send(api_action_name).where(action_type: 'serve_file').reorder(:created_at).last
+                      else
+                        @api_namespace.send(api_action_name).where(action_type: 'serve_file').last
+                      end
     return if serve_file_action.nil?
 
     serve_file_action.update(lifecycle_stage: 'executing')
@@ -32,10 +40,11 @@ module ApiActionable
     flash[:notice] = @api_namespace.api_form.success_message || 'Api resource was successfully updated.'
 
     if @redirect_action.present?
-      if @redirect_action.update!(lifecycle_stage: 'complete', lifecycle_message: @redirect_action.redirect_url.to_s)
-        redirect_to @redirect_action.redirect_url and return
+      redirect_url = @redirect_action.dynamic_url? ? @redirect_action.redirect_url_evaluated : @redirect_action.redirect_url
+      if @redirect_action.update!(lifecycle_stage: 'complete', lifecycle_message: redirect_url)
+        redirect_to redirect_url and return
       else
-        @redirect_action.update!(lifecycle_stage: 'failed', lifecycle_message: @redirect_action.redirect_url.to_s)
+        @redirect_action.update!(lifecycle_stage: 'failed', lifecycle_message: redirect_url)
         execute_error_actions
       end
     end
@@ -84,5 +93,10 @@ module ApiActionable
     @api_namespace.send(action_name).each do |action|
       @api_resource.send(action_name).create(action.attributes.merge(custom_message: action.custom_message.to_s).except("id", "created_at", "updated_at", "api_namespace_id"))
     end
+  end
+
+  def load_api_actions_from_api_resource
+    @redirect_action = @api_resource.send(api_action_name).where(action_type: 'redirect').reorder(:created_at).last if @redirect_action.present?
+    @serve_file_action = @api_resource.send(api_action_name).where(action_type: 'serve_file').reorder(:created_at).last if @serve_file_action.present?
   end
 end
