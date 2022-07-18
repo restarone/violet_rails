edac11dd7245877063d76005827d815c9f3f5b71
Prashant Khadka <prashant.khadka052@gmail.com>
2022-07-18 09:44:48 +0545

    fix(): schema change

--- db/schema.rb
+++ db/schema.rb
@@ -123,8 +123,8 @@ ActiveRecord::Schema.define(version: 2022_07_09_172402) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "custom_headers"
-    t.text "method_definition"
     t.string "http_method"
+    t.text "method_definition"
     t.text "email_subject"
     t.integer "redirect_type", default: 0
     t.index ["api_namespace_id"], name: "index_api_actions_on_api_namespace_id"
