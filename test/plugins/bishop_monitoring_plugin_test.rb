cb89cbbed6510671334bfca6491f887b647abb86
donrestarone <shashikejayatunge@gmail.com>
2022-07-18 21:27:35 -0400

    extract out test for clarity

--- test/plugins/bishop_monitoring_plugin_test.rb
+++ test/plugins/bishop_monitoring_plugin_test.rb
@@ -6,7 +6,7 @@ class BishopMonitoringPluginTest < ActionDispatch::IntegrationTest
     @user.update(can_manage_api: true)
   end
 
-  test "#BishopMonitoring: does not log incident (send HTTP request + email) if HTTP endpoint error does not occur" do
+  test "#BishopMonitoring: sends HTTP request and does not log incident (resource creation + email) if HTTP endpoint error does not occur" do
     bishop_plugin = external_api_clients(:bishop_monitoring)
     api_namespace = api_namespaces(:monitoring_targets)
     bishop_request = stub_request(:get, api_namespace.api_resources.first.properties['url'])
@@ -23,20 +23,32 @@ class BishopMonitoringPluginTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "#BishopMonitoring: does log incident, send HTTP request and email if HTTP endpoint error occurs" do
+  test "#BishopMonitoring: sends HTTP request and does log incident (resource creation) if HTTP endpoint error does occur" do
     bishop_plugin = external_api_clients(:bishop_monitoring)
     api_namespace = api_namespaces(:monitoring_targets)
     bishop_request = stub_request(:get, api_namespace.api_resources.first.properties['url'])
       .to_return(status: 500, body: "Gateway unavailable")
 
     sign_in(@user)
-    # ideally, should be +1
+    # TODO: fix to ensure that only 1 API resource is created (https://github.com/restarone/violet_rails/issues/584)
     assert_changes 'ApiResource.count' do
-      # TODO: this should send the email, but we dont fire it from the model context
-      assert_no_changes "ApiActionMailer.deliveries.size" do          
-        get start_api_namespace_external_api_client_path(api_namespace_id: api_namespace.id, id: bishop_plugin.id)
-        Sidekiq::Worker.drain_all
-      end
+      get start_api_namespace_external_api_client_path(api_namespace_id: api_namespace.id, id: bishop_plugin.id)
+      Sidekiq::Worker.drain_all
     end
   end
+
+  test "#BishopMonitoring: does log incident, send HTTP request and email if HTTP endpoint error occurs" do
+    skip("# TODO: this should send the email, but we dont fire it from the model context (https://github.com/restarone/violet_rails/issues/584)")
+    bishop_plugin = external_api_clients(:bishop_monitoring)
+    api_namespace = api_namespaces(:monitoring_targets)
+    bishop_request = stub_request(:get, api_namespace.api_resources.first.properties['url'])
+      .to_return(status: 500, body: "Gateway unavailable")
+
+    sign_in(@user)
+    assert_changes "ApiActionMailer.deliveries.size" do          
+      get start_api_namespace_external_api_client_path(api_namespace_id: api_namespace.id, id: bishop_plugin.id)
+      Sidekiq::Worker.drain_all
+    end
+  end
+
 end
