8bb31d78431304956e71aa8a24137dae0dcc73d2
Prashant Khadka <prashant.khadka052@gmail.com>
2022-08-01 13:04:01 +0545

    fea(): make changes to SyncAttributeToApiNamespace plugin to accept all provided value unless the metadata is absent

--- test/plugins/sync_attribute_to_api_namespace_plugin_test.rb
+++ test/plugins/sync_attribute_to_api_namespace_plugin_test.rb
@@ -207,4 +207,77 @@ class SyncAttributeToApiNamespacePluginTest < ActiveSupport::TestCase
     expected_message = 'The provided attribute is already defined in the ApiNamespace'
     assert_match expected_message, @sync_attribute_to_api_namespace_plugin.reload.error_message
   end
+
+  test "returns error if the ATTRIBUTE_NAME is not provided" do
+    metadata = {
+      'DEFAULT_VALUE' => 'test_value',
+      'PLACEHOLDER_VALUE' => 'Enter you new_attribute value here.',
+    }
+    @sync_attribute_to_api_namespace_plugin.update!(metadata: metadata)
+    api_resource = api_resources(:one)
+
+    (1..5).each do
+      new_api_resource = api_resource.dup
+      new_api_resource.save!
+    end
+
+    # @api_namespace already has the provided new-attribute
+    new_properties = @api_namespace.properties.merge('new_attribute' => 'test 1')
+    @api_namespace.update!(properties: new_properties)
+
+    assert_includes @api_namespace.properties.keys, 'new_attribute'
+
+    perform_enqueued_jobs do
+      assert_no_difference 'ApiNamespace.count' do
+        assert_no_difference 'ApiResource.count' do
+          assert_no_changes -> { @api_namespace.reload.properties['new_attribute'] } do
+            @sync_attribute_to_api_namespace_plugin.run
+            Sidekiq::Worker.drain_all
+          end
+        end
+      end
+    end
+
+    expected_message = 'ATTRIBUTE_NAME is missing!'
+    assert_match expected_message, @sync_attribute_to_api_namespace_plugin.reload.error_message
+  end
+
+  test "adds provided new attribute to api namespace and backfills the default value: false(boolean) to its api-resources successfully" do
+    metadata = {
+      'ATTRIBUTE_NAME' => 'new_attribute',
+      'DEFAULT_VALUE' => false,
+      'PLACEHOLDER_VALUE' => 'Enter you new_attribute value here.',
+    }
+    @sync_attribute_to_api_namespace_plugin.update!(metadata: metadata)
+    api_resource = api_resources(:one)
+
+    (1..5).each do
+      new_api_resource = api_resource.dup
+      new_api_resource.save!
+    end
+
+    # Initially, the api_namespace and its api-resources does not have the new 'new_attribute'
+    refute_includes @api_namespace.properties.keys, 'new_attribute'
+    @api_namespace.api_resources.each do |resource|
+      refute_includes resource.properties.keys, 'new_attribute'
+    end
+
+    perform_enqueued_jobs do
+      assert_no_difference 'ApiNamespace.count' do
+        assert_no_difference 'ApiResource.count' do
+          @sync_attribute_to_api_namespace_plugin.run
+          Sidekiq::Worker.drain_all
+        end
+      end
+    end
+
+    assert_equal metadata['PLACEHOLDER_VALUE'], @api_namespace.reload.properties['new_attribute']
+    # All api-resources are backfilled with provided 'DEFAULT_VALUE'
+    @api_namespace.reload.api_resources.each do |resource|
+      assert_equal metadata['DEFAULT_VALUE'], resource.properties['new_attribute']
+    end
+
+    # The newly added attribute is non-renderable
+    assert_equal '0', @api_form.reload.properties['new_attribute']['renderable']
+  end
 end
