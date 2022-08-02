f60e7ae1020ae56671ecf95bac66fd110d90b8bf
Pralish Kayastha <pralishkayastha@gmail.com>
2022-08-02 15:35:01 +0545

    conditional render

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,2 +1,9 @@
 module ContentHelper
+  def login_helper(if_identifier, else_identifier = nil)
+    if current_user
+        cms_snippet_render(if_identifier)
+    elsif identifier_2
+        cms_snippet_render(else_identifier)
+    end
+  end
 end
