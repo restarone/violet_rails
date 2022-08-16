abf504e084b9299e4fc3983402045ae8218ee329
Prashant Khadka <prashant.khadka052@gmail.com>
2022-08-16 09:44:27 +0545

    feat(): make current-user and current-visit available in background job

--- app/models/api_resource.rb
+++ app/models/api_resource.rb
@@ -110,11 +110,11 @@ class ApiResource < ApplicationRecord
 
   def execute_create_api_actions
     clone_api_actions('create_api_actions')
-    FireApiActionsJob.perform_async(self.id, 'create_api_actions')
+    FireApiActionsJob.perform_async(self.id, 'create_api_actions', Current.user&.id, Current.visit&.id)
   end
 
   def execute_update_api_actions
     clone_api_actions('update_api_actions')
-    FireApiActionsJob.perform_async(self.id, 'update_api_actions')
+    FireApiActionsJob.perform_async(self.id, 'update_api_actions', Current.user&.id, Current.visit&.id)
   end
 end
--- app/sidekiq/fire_api_actions_job.rb
+++ app/sidekiq/fire_api_actions_job.rb
@@ -1,10 +1,15 @@
 class FireApiActionsJob
   include Sidekiq::Job
 
-  def perform(api_resource_id, action_class)
-    api_resource = ApiResource.find(api_resource_id)
+  def perform(api_resource_id, action_class, current_user_id, current_visit_id)
+    current_user = User.find_by(id: current_user_id)
+    current_visit = Ahoy::Visit.find_by(id: current_visit_id)
 
-    api_resource.execute_model_context_api_actions(action_class)
+    Current.set(user: current_user, visit: current_visit) do
+      api_resource = ApiResource.find(api_resource_id)
+  
+      api_resource.execute_model_context_api_actions(action_class)
+    end
   end
 end
   
