f6e24d1c120500e8ad2d26d66f4cdc236fcfc6b5
Prashant <alish.khadka@gmail.com>
2022-08-17 06:05:56 +0545

    [feature]: Fire api actions from model (#1012)
    Addresses: https://github.com/restarone/violet_rails/issues/584
    
    Co-authored-by: Pralish Kayastha <pralishkayastha@gmail.com>

--- test/controllers/resource_controller_test.rb
+++ test/controllers/resource_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class ResourceControllerTest < ActionDispatch::IntegrationTest
   setup do
     @api_namespace = api_namespaces(:one)
+    Sidekiq::Testing.fake!
   end
 
   test 'should allow #create' do
@@ -22,6 +23,7 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
           perform_enqueued_jobs do
             post api_namespace_resource_index_url(api_namespace_id: @api_namespace.id, params: payload)
             assert_response :success
+            Sidekiq::Worker.drain_all
           end
         end
       end
@@ -222,8 +224,11 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
       }
     }
 
-    assert_difference "@api_namespace.api_resources.count", +1 do
-      post api_namespace_resource_index_url(api_namespace_id: @api_namespace.id), params: payload
+    perform_enqueued_jobs do
+      assert_difference "@api_namespace.api_resources.count", +1 do
+        post api_namespace_resource_index_url(api_namespace_id: @api_namespace.id), params: payload
+        Sidekiq::Worker.drain_all
+      end
     end
 
     # Provided current_user & current_visit variable are available through send_web_request api-action
@@ -562,10 +567,14 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
           }
       }
     }
-    assert_difference "api_namespace.api_resources.count", +1 do
-      # Custom Action creates a new user
-      assert_difference "User.count", +1 do
-        post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+
+    perform_enqueued_jobs do
+      assert_difference "api_namespace.api_resources.count", +1 do
+        # Custom Action creates a new user
+        assert_difference "User.count", +1 do
+          post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+          Sidekiq::Worker.drain_all
+        end
       end
     end
   end
@@ -590,10 +599,14 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
           }
       }
     }
-    assert_difference "api_namespace.api_resources.count", +1 do
-      # Total 3 Custom Action. Each creates a new user
-      assert_difference "User.count", +3 do
-        post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+
+    perform_enqueued_jobs do
+      assert_difference "api_namespace.api_resources.count", +1 do
+        # Total 3 Custom Action. Each creates a new user
+        assert_difference "User.count", +3 do
+          post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+          Sidekiq::Worker.drain_all
+        end
       end
     end
 
@@ -628,10 +641,14 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
           }
       }
     }
-    assert_difference "api_namespace.api_resources.count", +1 do
-      # Total 3 Custom Action. Each sends an email.
-      assert_difference "ActionMailer::Base.deliveries.count", +3 do
-        post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+
+    perform_enqueued_jobs do
+      assert_difference "api_namespace.api_resources.count", +1 do
+        # Total 3 Custom Action. Each sends an email.
+        assert_difference "ActionMailer::Base.deliveries.count", +3 do
+          post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+          Sidekiq::Worker.drain_all
+        end
       end
     end
 
@@ -666,8 +683,11 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
           }
       }
     }
-    assert_difference "api_namespace.api_resources.count", +1 do
-      post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+    perform_enqueued_jobs do
+      assert_difference "api_namespace.api_resources.count", +1 do
+        post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+        Sidekiq::Worker.drain_all
+      end
     end
 
     api_namespace = @controller.view_assigns['api_namespace']
@@ -714,10 +734,14 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
           }
       }
     }
-    assert_difference "api_namespace.api_resources.count", +1 do
-      # Total 3 Custom Action & 1 Send-Email Action. Each sends an email.
-      assert_difference "ActionMailer::Base.deliveries.count", +4 do
-        post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+
+    perform_enqueued_jobs do
+      assert_difference "api_namespace.api_resources.count", +1 do
+        # Total 3 Custom Action & 1 Send-Email Action. Each sends an email.
+        assert_difference "ActionMailer::Base.deliveries.count", +4 do
+          post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+          Sidekiq::Worker.drain_all
+        end
       end
     end
 
@@ -743,7 +767,7 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
     api_namespace = api_namespaces(:three)
     api_namespace.api_form.update(properties: { 'name': {'label': 'name', 'placeholder': 'Test', 'type_validation': 'tel'}})
     custom_api_action_1 = api_actions(:create_custom_api_action_three)
-    custom_api_action_1.update!(position: 0, method_definition: "User.invite!({email: 'custom_action_0@restarone.com'}, current_user)")
+    custom_api_action_1.update!(position: 5, method_definition: "User.invite!({email: 'custom_action_0@restarone.com'}, current_user)")
 
     custom_custom_action_2 = api_actions(:create_custom_api_action_three).dup
     custom_custom_action_2.method_definition = "User.invite!({email: 'custom_action_1@restarone.com'}, current_user)"
@@ -788,10 +812,14 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
           }
       }
     }
-    assert_difference "api_namespace.api_resources.count", +1 do
-      # Total 2 Custom Action & 1 Send-Email Action. Each sends an email.
-      assert_difference "ActionMailer::Base.deliveries.count", +3 do
-        post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+
+    perform_enqueued_jobs do
+      assert_difference "api_namespace.api_resources.count", +1 do
+        # Total 2 Custom Action & 1 Send-Email Action. Each sends an email.
+        assert_difference "ActionMailer::Base.deliveries.count", +3 do
+          post api_namespace_resource_index_url(api_namespace_id: api_namespace.id), params: payload
+          Sidekiq::Worker.drain_all
+        end
       end
     end
 
@@ -800,7 +828,10 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
     api_resource = @controller.view_assigns['api_resource']
 
     # Different type of ApiActions are executed in the defined order
-    assert_equal ApiAction::EXECUTION_ORDER, api_resource.create_api_actions.reorder(nil).order(updated_at: :asc).pluck(:action_type).uniq
+    # model level actions are executed according to the defined order asynchronously: EXECUTION_ORDER[:model_level]
+    assert_equal ApiAction::EXECUTION_ORDER[:model_level], api_resource.create_api_actions.reorder(nil).order(updated_at: :asc).where(action_type: ApiAction::EXECUTION_ORDER[:model_level]).pluck(:action_type).uniq
+    # controller level actions are executed according to the defined order: EXECUTION_ORDER[:controller_level]
+    assert_equal ApiAction::EXECUTION_ORDER[:controller_level], api_resource.create_api_actions.reorder(nil).order(updated_at: :asc).where(action_type: ApiAction::EXECUTION_ORDER[:controller_level]).pluck(:action_type).uniq
 
     # Custom Api Action are executed according to their position
     custom_actions = api_resource.reload.create_api_actions.where(action_type: 'custom_action').reorder(nil)
--- test/plugins/bishop_tls_monitoring_plugin_test.rb
+++ test/plugins/bishop_tls_monitoring_plugin_test.rb
@@ -4,12 +4,61 @@ class BishopTlsMonitoringPluginTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:public)
     @user.update(can_manage_api: true)
+    WebMock.allow_net_connect!(net_http_connect_on_start: true)
   end
 
-  test "#BishopTlsMonitoring: sends HTTP request and email when HTTPS endpoint has near expiry TLS certificate" do
-    skip("need to figure out how to stub the Net::HTTP")
-    bishop_plugin = external_api_clients(:bishop_tls_monitoring)
-    api_resource = api_namespaces(:monitoring_targets)
+  test "#BishopTlsMonitoring: does not send HTTP request and email (api-actions) when HTTPS endpoint TLS certificate does not expire within 2 weeks" do
+    Net::HTTP.any_instance.stubs(:peer_cert).returns(stub(:not_after => (Time.now + 2.months)))
+
+    bishop_tls_plugin = external_api_clients(:bishop_tls_monitoring)
+    api_namespace = api_namespaces(:monitoring_targets)
+    bishop_request = stub_request(:get, api_namespace.api_resources.first.properties['url'])
+      .to_return(status: 200, body: "OK")
+
+    sign_in(@user)
+    perform_enqueued_jobs do
+      assert_no_difference 'ApiResource.count' do
+        assert_no_difference "ApiActionMailer.deliveries.size" do          
+          get start_api_namespace_external_api_client_path(api_namespace_id: api_namespace.id, id: bishop_tls_plugin.id)
+          Sidekiq::Worker.drain_all
+        end
+      end
+    end
+  end
+
+  test "#BishopTlsMonitoring: does log incident, send HTTP request and email if HTTP endpoint has TLS certificate expiry within 2 weeks" do
+    Net::HTTP.any_instance.stubs(:peer_cert).returns(stub(:not_after => (Time.now + 1.weeks)))
+
+    bishop_tls_plugin = external_api_clients(:bishop_tls_monitoring)
+    api_namespace = api_namespaces(:monitoring_targets)
+    bishop_request = stub_request(:get, api_namespace.api_resources.first.properties['url'])
+      .to_return(status: 200, body: "OK")
+
+    api_actions(:create_api_action_plugin_bishop_monitoring_web_request).update!(
+      payload_mapping: { 'content': 'test body'},
+      custom_headers: { 'accept': 'application/json'},
+      bearer_token: 'TEST',
+      request_url: 'http://www.discord.com',
+      http_method: 'post'
+    )
+    
+    target_namespace = api_namespaces(:monitoring_target_incident)
+    discord_request = stub_request(:post, "http://www.discord.com").to_return(status: 200, body: 'Success.')
+
+    sign_in(@user)
+    
+    # ActiveRecords will be created times the defined max-retries of plugin
+    assert_difference 'ApiResource.count', +(1 * bishop_tls_plugin.max_retries)  do
+      assert_difference 'ApiAction.count', +(target_namespace.create_api_actions.count * bishop_tls_plugin.max_retries) do
+        # 1 send-email (times the defined max-retries of plugin) create-api-action is executed
+        assert_difference "ActionMailer::Base.deliveries.count", +(1 * bishop_tls_plugin.max_retries) do
+          get start_api_namespace_external_api_client_path(api_namespace_id: api_namespace.id, id: bishop_tls_plugin.id)
+          Sidekiq::Worker.drain_all
+        end
+      end
+    end
+
+    assert_requested discord_request
   end
   
 end
