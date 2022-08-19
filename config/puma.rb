a96d1dd2acc0d261ab35d8bfcb3ec49ef332c38c
Prashant Khadka <prashant.khadka052@gmail.com>
2022-08-19 13:19:44 +0545

    feat(): running sidekiq on heroku frre dyno

--- config/puma.rb
+++ config/puma.rb
@@ -24,20 +24,34 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+if environment == "development"
+  # Specifies the number of `workers` to boot in clustered mode.
+  # Workers are forked web server processes. If using threads and workers together
+  # the concurrency of the application would be max `threads` * `workers`.
+  # Workers do not work on JRuby or Windows (both of which do not support
+  # processes).
+  #
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+  
+  # Use the `preload_app!` method when specifying a `workers` number.
+  # This directive tells Puma to first boot the application and load code
+  # before forking the application. This takes advantage of Copy On Write
+  # process behavior so workers use less memory.
+  #
+  preload_app!
+  
+  before_fork do 
+      @sidekiq_pid ||= spawn('bundle exec sidekiq -t 25')
+  end
+  
+  on_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  end
+  
+  on_restart do
+    Sidekiq.redis.shutdown { |conn| conn.close }
+  end
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
