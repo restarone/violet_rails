88a430bba5f5a05cb95b77aa11bb2fae761ad8e4
Prashant Khadka <prashant.khadka052@gmail.com>
2022-08-23 14:03:31 +0545

    feat(): changes to category's model and controller to adjust for other entities

---
+++ app/models/comfy/cms/category.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Comfy::Cms::Category < ActiveRecord::Base
+
+  self.table_name = "comfy_cms_categories"
+
+  NON_SITE_ENTITIES = ['User', 'MessageThres']
+
+  # -- Relationships --------------------------------------------------------
+  belongs_to :site, optional: true
+  has_many :categorizations,
+    dependent: :destroy
+
+  # -- Validations ----------------------------------------------------------
+  validates :label,
+    presence:   true,
+    uniqueness: { scope: %i[categorized_type site_id] }
+  validates :categorized_type,
+    presence:   true
+
+  # -- Scopes ---------------------------------------------------------------
+  scope :of_type, ->(type) {
+    where(categorized_type: type)
+  }
+
+end
+  
