6e57806e5171e22584f3562495f5e62dd19d09f1
Ayon95 <mushfiqurrahman78@yahoo.com>
2022-08-28 19:41:11 -0400

    change cookie consent layout and add overlay

--- db/migrate/20220824121504_add_cookies_consent_ui_to_subdomain.rb
+++ db/migrate/20220824121504_add_cookies_consent_ui_to_subdomain.rb
@@ -1,12 +1,16 @@
 class AddCookiesConsentUiToSubdomain < ActiveRecord::Migration[6.1]
   COOKIE_CONSENT_UI = 
   "<div class=\"cookies-consent\">
-    <h2 class=\"cookies-consent__title\">We Value Your Privacy</h2>
-    <p>
-      We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking \"Accept All\", you consent to our use of cookies.
-    </p>
-    <a class=\"btn btn-primary mr-2 mb-2 mb-sm-0\" href=\"/cookies?cookies=true\">Accept All</a>
-    <a class=\"btn btn-outline-primary mb-2 mb-sm-0\" href=\"/cookies?cookies=false\">Reject All</a>
+    <div class=\"cookies-consent__text-content\">
+      <h2 class=\"cookies-consent__title\">We Value Your Privacy</h2>
+      <p class=\"mb-4 mb-md-0\">
+        We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking \"Accept All\", you consent to our use of cookies.
+      </p>
+    </div>
+    <div class=\"cookies-consent__buttons-container\">
+      <a class=\"btn btn-primary mb-3\" href=\"/cookies?cookies=true\">Accept All</a>
+      <a class=\"btn btn-outline-primary\" href=\"/cookies?cookies=false\">Reject All</a>
+    </div>  
   </div>"
 
   def change
