8ccca9fdc342403594063a87592b6e41b1234846
Prashant Khadka <prashant.khadka052@gmail.com>
2022-09-10 18:41:06 +0545

    chore(): testing brakemann error

--- app/controllers/comfy/admin/cms/layouts_controller.rb
+++ app/controllers/comfy/admin/cms/layouts_controller.rb
@@ -67,7 +67,9 @@ protected
   end
 
   def layout_params
-    params.require(:layout).permit(:label, :identifier, :parent_id, :app_layout, :content, :css, :js, category_ids: [])
+    # params.require(:layout).permit(:label, :identifier, :parent_id, :app_layout, :content, :css, :js, category_ids: [])
+    # params.fetch(:layout, {}).permit(:label, :identifier, :parent_id, :app_layout, :content, :css, :js, category_ids: [])
+    params.fetch(:layout, {}).permit!
   end
 
 end
