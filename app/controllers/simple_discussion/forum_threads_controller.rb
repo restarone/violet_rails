e4ec24f3a6c9fe16e62fff86e3f24a61311dd0f5
ashishthesatan <ashishthesatan@gmail.com>
2022-09-11 08:40:39 +0545

    fixed issues on threads controller

--- app/controllers/simple_discussion/forum_threads_controller.rb
+++ app/controllers/simple_discussion/forum_threads_controller.rb
@@ -104,16 +104,5 @@ class SimpleDiscussion::ForumThreadsController < SimpleDiscussion::ApplicationCo
     params.require(:forum_thread).permit(:title, :forum_category_id, forum_posts_attributes: [:body])
   end
 
-  def show
-    byebug
-    params[:q] ||= {}
-    @message_threads_q = if params[:categories].present?
-      MessageThread.includes(:categories).for_category(params[:categories]).all.includes(:messages).ransack(params[:q])
-    else
-      MessageThread.all.includes(:messages).ransack(params[:q])
-    end
-
-    @message_threads_q.sorts = ['created_at desc'] if @message_threads_q.sorts.empty?
-    @message_threads = @message_threads_q.result(distinct: true).paginate(page: params[:page], per_page: params[:per_page] || 10)
-  end
+  
 end
