a040e6f4c6b06bcd31b000b937d0b0040d1b288a
peaceofmind123 <ashishthesatan@gmail.com>
2022-09-12 22:00:04 +0545

    Search forum complete (#1097)
    * implemented search controller on forum threads
    
    * basic search scaffolding done
    
    * title search working
    
    * search query done
    
    * remove debug flags
    
    * fixed issues on threads controller
    
    * done with the entire search functionality
    
    * final commit
    
    * remove unnecessary changes
    
    * added custom validator for rich text body

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,14 +1,25 @@
+class RichTextBodyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.body.blank?
+      record.errors.add :body, " can't be blank"
+    end
+  end
+end
 class ForumPost < ApplicationRecord
+  include ActiveModel::Validations
+
   belongs_to :forum_thread, counter_cache: true, touch: true
   belongs_to :user, optional: true
 
   validates :body, presence: true
+  validates_with RichTextBodyValidator
   has_rich_text :body
   has_one :body, class_name: 'ActionText::RichText', as: :record
   scope :sorted, -> { order(:created_at) }
 
   after_update :solve_forum_thread, if: :solved?
 
+  
   def solve_forum_thread
     forum_thread.update(solved: true)
   end
