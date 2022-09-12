8914a0900813b2df8c519259aa052b6629690823
Ajay Shrestha <er.ajaystha@gmail.com>
2022-09-12 17:17:17 +0545

    feat: search threads by title and post body

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -4,7 +4,8 @@ class ForumPost < ApplicationRecord
 
   validates :body, presence: true
   has_rich_text :body
-
+  # Ref: https://stackoverflow.com/questions/59575397/search-text-in-actiontext-attribute/60138981#60138981
+  has_one :action_text_rich_text, class_name: 'ActionText::RichText', as: :record
   scope :sorted, -> { order(:created_at) }
 
   after_update :solve_forum_thread, if: :solved?
