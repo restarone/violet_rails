ca050440f91495711126feaea0f2513436084864
Pralish Kayastha <50227291+Pralish@users.noreply.github.com>
2022-09-15 02:54:28 +0545

    [Bug Fix] Cannot create new layout (#1106) (#1107)
    Addresses: https://github.com/restarone/violet_rails/issues/1089
    
    
    https://user-images.githubusercontent.com/25191509/189512814-53ccb975-d6d7-4831-a329-edbb1263c66e.mp4
    
    Co-authored-by: Prashant <alish.khadka@gmail.com>
    
    Co-authored-by: Don Restarone <35935196+donrestarone@users.noreply.github.com>
    Co-authored-by: Prashant <alish.khadka@gmail.com>

--- test/controllers/admin/comfy/layouts_controller_test.rb
+++ test/controllers/admin/comfy/layouts_controller_test.rb
@@ -19,17 +19,85 @@ class Comfy::Admin::Cms::LayoutsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'allows #index and redirects to #new if no layouts are present' do
+    site = comfy_cms_sites(:public)
+    Comfy::Cms::Layout.delete_all
+    
+    user = users(:public)
+    user.update(can_access_admin: true, can_manage_web: true)
+    sign_in(user)
+    get comfy_admin_cms_site_layouts_url(site_id: site.id)
+
+    assert_response :redirect
+    assert_redirected_to action: :new
+  end
+
   test 'denies #index if not permissioned' do
     sign_in(@user)
     get comfy_admin_cms_site_layouts_url(subdomain: @restarone_subdomain, site_id: @site.id)
     assert_response :redirect
   end
 
+  test "allows #new if permissioned" do
+    @user.update(can_access_admin: true, can_manage_web: true)
+    sign_in(@user)
+    get new_comfy_admin_cms_site_layout_url(subdomain: @restarone_subdomain, site_id: @site.id)
+
+    assert_response :success
+    assert assigns(:layout)
+    assert_equal "{{ cms:wysiwyg content }}", assigns(:layout).content
+    assert_template :new
+    assert_select "form[action='/admin/sites/#{@site.id}/layouts']"
+  end
+
+  test "allows #new with parent if permissioned" do
+    site = comfy_cms_sites(:public)
+    layout = comfy_cms_layouts(:default)
+    layout.update_column(:app_layout, "application")
+
+    user = users(:public)
+    user.update(can_access_admin: true, can_manage_web: true)
+    sign_in(user)
+    get new_comfy_admin_cms_site_layout_url(site_id: site.id), params: { parent_id: layout.id }
+
+    assert_response :success
+    assert_equal layout.app_layout, assigns(:layout).app_layout
+    assert_template :new
+    assert_select "form[action='/admin/sites/#{site.id}/layouts']"
+  end
 
   test 'denies #new if not permissioned' do
     sign_in(@user)
     get new_comfy_admin_cms_site_layout_url(subdomain: @restarone_subdomain, site_id: @site.id)
     assert_response :redirect
+  end
+
+  test 'allows #edit if permissioned' do
+    site = comfy_cms_sites(:public)
+    layout = comfy_cms_layouts(:default)
+    
+    user = users(:public)
+    user.update(can_access_admin: true, can_manage_web: true)
+    sign_in(user)
+    get edit_comfy_admin_cms_site_layout_url(site_id: site.id, id: layout.id)
+
+    assert_response :success
+    assert assigns(:layout)
+    assert_template :edit
+    assert_select "form[action='/admin/sites/#{layout.site.id}/layouts/#{layout.id}']"
+  end
+
+  test 'denies #edit if layout not found' do
+    site = comfy_cms_sites(:public)
+    
+    user = users(:public)
+    user.update(can_access_admin: true, can_manage_web: true)
+    sign_in(user)
+    get edit_comfy_admin_cms_site_layout_url(site_id: site.id, id: "invalid")
+
+    assert_response :redirect
+    assert_redirected_to action: :index
+    assert_equal "Layout not found", flash[:danger]
   end
 
   test 'denies #edit if not permissioned' do
@@ -38,16 +106,96 @@ class Comfy::Admin::Cms::LayoutsControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
   end
 
+  test 'allows #update if permissioned' do
+    site = comfy_cms_sites(:public)
+    layout = comfy_cms_layouts(:default)
+
+    user = users(:public)
+    user.update(can_access_admin: true, can_manage_web: true)
+    sign_in(user)
+    put comfy_admin_cms_site_layout_url(site_id: site.id, id: layout.id), params: { layout: {
+      label:    "New Label",
+      content:  "New {{cms:page:content}}"
+    } }
+
+    assert_response :redirect
+    assert_redirected_to action: :edit, site_id: layout.site, id: layout
+    assert_equal "Layout updated", flash[:success]
+    layout.reload
+    assert_equal "New Label", layout.label
+    assert_equal "New {{cms:page:content}}", layout.content
+  end
+
+  test 'denies #update if proper details are not provided' do
+    site = comfy_cms_sites(:public)
+    layout = comfy_cms_layouts(:default)
+
+    user = users(:public)
+    user.update(can_access_admin: true, can_manage_web: true)
+    sign_in(user)
+    put comfy_admin_cms_site_layout_url(site_id: site.id, id: layout.id), params: { layout: {
+      identifier: ""
+    } }
+
+    assert_response :success
+    assert_template :edit
+    layout.reload
+    assert_not_equal "", layout.identifier
+    assert_equal "Failed to update layout", flash[:danger]
+  end
+
   test 'denies #update if not permissioned' do
     sign_in(@user)
     patch comfy_admin_cms_site_layout_url(subdomain: @restarone_subdomain, site_id: @site.id, id: @page.id)
     assert_response :redirect
   end
 
+  test 'allows #destroy if permissioned' do
+    site = comfy_cms_sites(:public)
+    layout = comfy_cms_layouts(:default)
+
+    user = users(:public)
+    user.update(can_access_admin: true, can_manage_web: true)
+
+    assert_difference "Comfy::Cms::Layout.count", -1 do
+      sign_in(user)
+      delete comfy_admin_cms_site_layout_url(site_id: site.id, id: layout.id)
+    end
+
+    assert_response :redirect
+    assert_redirected_to action: :index
+    assert_equal "Layout deleted", flash[:success]
+  end
+
   test 'denies #destroy if not permissioned' do
     sign_in(@user)
     delete comfy_admin_cms_site_layout_url(subdomain: @restarone_subdomain, site_id: @site.id, id: @page.id)
     assert_response :redirect
+  end
+
+  test 'allows #reorder' do
+    site = comfy_cms_sites(:public)
+    layout_one = comfy_cms_layouts(:default)
+    layout_two = site.layouts.create!(
+      label:      "test",
+      identifier: "test"
+    )
+    assert_equal 0, layout_one.position
+    assert_equal 1, layout_two.position
+
+    user = users(:public)
+    user.update(can_access_admin: true, can_manage_web: true)
+    sign_in(user)
+
+    put reorder_comfy_admin_cms_site_layouts_url(site_id: site.id), params: {
+      order: [layout_two.id, layout_one.id]
+    }
+    assert_response :success
+    layout_one.reload
+    layout_two.reload
+
+    assert_equal 1, layout_one.position
+    assert_equal 0, layout_two.position
   end
 
   test '#index: shows only layouts with provided categories' do
