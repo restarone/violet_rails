0f0e2cc8650cc6e37c5b182a027f5cc05b3a2feb
Pralish Kayastha <pralishkayastha@gmail.com>
2022-10-26 08:23:57 +0545

    test custom webhook verification

--- test/services/webhook/verification_test.rb
+++ test/services/webhook/verification_test.rb
@@ -4,26 +4,29 @@ class Webhook::VerificationTest  < ActiveSupport::TestCase
   setup do
     @external_api_client = external_api_clients(:webhook_drive_strategy)
     @request = ActionDispatch::Request.new({ "REQUEST_METHOD" => "POST", "HTTP_STRIPE_SIGNATURE" => "dummy_signature", "RAW_POST_DATA" => "{}" }) 
-    @webhook_verification_method = WebhookVerificationMethod.create(webhook_type: 'stripe', external_api_client_id: @external_api_client.id, webhook_secret: 'secret') 
   end
 
   test "stripe webhook type" do
     Webhook::VerificationMethod::Stripe.any_instance.stubs(:call).returns([true, 'success'])
+    webhook_verification_method = WebhookVerificationMethod.create(webhook_type: 'stripe', external_api_client_id: @external_api_client.id, webhook_secret: 'secret') 
 
-    verification_service = Webhook::Verification.new(@request, @webhook_verification_method)
+    verification_service = Webhook::Verification.new(@request, webhook_verification_method)
 
-    assert_equal @webhook_verification_method.webhook_type, verification_service.webhook_type
-    assert_equal @webhook_verification_method.webhook_secret, verification_service.secret_key
+    assert_equal webhook_verification_method, verification_service.verification_method
+    assert_equal @request, verification_service.request
 
     assert_equal [true, 'success'], verification_service.call
   end
 
-  test "should return method not defined message if unlisted webhook_type is used" do
-    Webhook::VerificationMethod::Stripe.any_instance.stubs(:call).returns([true, 'success'])
+  test "custom webhook type" do
+    Webhook::VerificationMethod::Custom.any_instance.stubs(:call).returns([true, 'success'])
+    webhook_verification_method = WebhookVerificationMethod.create(webhook_type: 'custom', external_api_client_id: @external_api_client.id, webhook_secret: 'secret') 
 
-    verification_service = Webhook::Verification.new(@request, @webhook_verification_method)
-    verification_service.webhook_type = 'random'
+    verification_service = Webhook::Verification.new(@request, webhook_verification_method)
 
-    assert_equal [false, 'Webhook verification method not defined'], verification_service.call
+    assert_equal webhook_verification_method, verification_service.verification_method
+    assert_equal @request, verification_service.request
+
+    assert_equal [true, 'success'], verification_service.call
   end
 end
