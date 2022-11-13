aa5a454871806633b0f1b2cdc82015bb24884155
Ajay Shrestha <er.ajaystha@gmail.com>
2022-11-13 20:55:36 +0545

    setting token timeout to 120 seconds

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -41,6 +41,7 @@ end
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
+Devise.otp_allowed_drift = 120 # value in seconds
 Devise.setup do |config|
   config.warden do |manager|
     manager.default_strategies(:scope => :user).unshift :two_factor_authenticatable
