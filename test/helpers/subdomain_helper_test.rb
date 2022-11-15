f8a3d889441fa0fd5f4e24a7364adfee800aa3dd
Pralish Kayastha <pralishkayastha@gmail.com>
2022-11-15 18:01:33 +0545

    fix test case

--- test/helpers/subdomain_helper_test.rb
+++ test/helpers/subdomain_helper_test.rb
@@ -7,38 +7,40 @@ class SubDomainHelperTest < ActionView::TestCase
         @snippet = comfy_cms_snippets(:public)
         @cms_site = comfy_cms_sites(:public)
     
-        @api_namespace = api_namespaces(:one) 
-        @api_resource= ApiResource.create(api_namespace_id: @api_namespace.id, properties: { title: 'test title', description: 'test description', image: 'image_link_test' })
+        @api_namespace_1 = api_namespaces(:one) 
+        @api_resource_1= ApiResource.create(api_namespace_id: @api_namespace_1.id, properties: { title: 'test title', description: 'test description', image: 'image_link_test' })
         test_image = Rails.root.join("test/fixtures/files/fixture_image.png")
         file = Rack::Test::UploadedFile.new(test_image, "image/png")
 
-        NonPrimitiveProperty.create!(label: "image", field_type: "file", api_namespace_id: @api_namespace.id)
-        @prop = NonPrimitiveProperty.create!(label: "image", field_type: "file", attachment: file, api_resource_id: @api_resource.id)
+        NonPrimitiveProperty.create!(label: "image", field_type: "file", api_namespace_id: @api_namespace_1.id)
+        @prop = NonPrimitiveProperty.create!(label: "image", field_type: "file", attachment: file, api_resource_id: @api_resource_1.id)
         Current.user = @user
     end
 
     test 'og_metadata - update meta data from social_share_metadata in show page if social_share_metadata is set' do
         @current_user = @user
-        params[:id] = @api_resource.id
+        params[:id] = @api_resource_1.id
         @is_show_page = true
-        @api_namespace.social_share_metadata = {
+        @api_namespace_1.update(social_share_metadata: {
           title: 'title',
           description: 'description',
           image: 'image'
-        }
-        snippet = Comfy::Cms::Snippet.create(site_id: @cms_site.id, label: 'clients', identifier: "#{@api_namespace.slug}-show", position: 0, content: "<%= @api_resource.properties['title'] %>")
-        response = og_metadata(@is_show_page)
-        assert_equal @prop.file_url, @api_resource.props['image'].file_url
-        assert_equal response[:title], @api_resource.props['title']
-        assert_equal response[:description], @api_resource.props['description']
+        })
+        @api_resource_1.reload
+        snippet = Comfy::Cms::Snippet.create(site_id: @cms_site.id, label: 'clients', identifier: "#{@api_namespace_1.slug}-show", position: 0, content: "<%= @api_resource.properties['title'] %>")
+        response = og_metadata(@is_show_page, @api_resource_1)
+    
+        assert_equal @prop.file_url, @api_resource_1.props['image'].file_url
+        assert_equal response[:title], @api_resource_1.props['title']
+        assert_equal response[:description], @api_resource_1.props['description']
     end
     
     test 'og_metadata - should display global metadata if social_share_metadata is not set' do
         @current_user = @user
-        params[:id] = @api_resource.id
+        params[:id] = @api_resource_1.id
         @is_show_page = true
-        snippet = Comfy::Cms::Snippet.create(site_id: @cms_site.id, label: 'clients', identifier: "#{@api_namespace.slug}-show", position: 0, content: "<%= @api_resource.properties['title'] %>")
-        response = og_metadata(@is_show_page)
+        snippet = Comfy::Cms::Snippet.create(site_id: @cms_site.id, label: 'clients', identifier: "#{@api_namespace_1.slug}-show", position: 0, content: "<%= @api_resource.properties['title'] %>")
+        response = og_metadata(@is_show_page, @api_resource_1)
         assert_equal 'public', response[:title]
         assert_equal 'public', response[:description]
         assert_nil response[:image]
@@ -46,17 +48,37 @@ class SubDomainHelperTest < ActionView::TestCase
 
     test 'og_metadata - should display global metadata if its not a show page' do
         @current_user = @user
-        params[:id] = @api_resource.id
+        params[:id] = @api_resource_1.id
         @is_show_page = false
-        @api_namespace.social_share_metadata = {
+        @api_namespace_1.social_share_metadata = {
             title: 'title',
             description: 'description',
             image: 'image'
           }
-        snippet = Comfy::Cms::Snippet.create(site_id: @cms_site.id, label: 'clients', identifier: "#{@api_namespace.slug}-show", position: 0, content: "<%= @api_resource.properties['title'] %>")
-        response = og_metadata(@is_show_page)
+        snippet = Comfy::Cms::Snippet.create(site_id: @cms_site.id, label: 'clients', identifier: "#{@api_namespace_1.slug}-show", position: 0, content: "<%= @api_resource.properties['title'] %>")
+        response = og_metadata(@is_show_page,  @api_resource_1)
         assert_equal 'public', response[:title]
         assert_equal 'public', response[:description]
         assert_nil response[:image]
+    end
+
+    test 'og_metadata - should not fail because of @api_namespace instance variable' do
+        @current_user = @user
+        params[:id] = @api_resource_1.id
+        @is_show_page = true
+        @api_namespace_1.update(social_share_metadata: {
+          title: 'title',
+          description: 'description',
+          image: 'image'
+        })
+        @api_namespace = api_namespaces(:two)
+        @api_resource = api_resources(:two)
+        @api_resource_1.reload
+        snippet = Comfy::Cms::Snippet.create(site_id: @cms_site.id, label: 'clients', identifier: "#{@api_namespace_1.slug}-show", position: 0, content: "<%= @api_resource.properties['title'] %>")
+        response = og_metadata(@is_show_page, @api_resource_1)
+    
+        assert_equal @prop.file_url, @api_resource_1.props['image'].file_url
+        assert_equal response[:title], @api_resource_1.props['title']
+        assert_equal response[:description], @api_resource_1.props['description']
     end
 end
