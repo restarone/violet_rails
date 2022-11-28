9da8b698d26d0ca9ed90891957929e8696ce1636
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2022-11-28 22:03:48 +0545

    feat(): fix ahoy cookies being set for activestorage url

---
+++ app/controllers/concerns/ahoy_controller_patch.rb
@@ -0,0 +1,15 @@
+module AhoyControllerPatch
+	def set_ahoy_cookies
+		if Ahoy.cookies && tracking_enabled?
+			ahoy.set_visitor_cookie
+			ahoy.set_visit_cookie
+		else
+			# delete cookies if exist
+			ahoy.reset
+		end
+	end
+
+	def tracking_enabled?
+		Subdomain.current.tracking_enabled && cookies[:cookies_accepted] == 'true'
+	end
+end
