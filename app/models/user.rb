b627b73786cf7326b31b628852a51c952d08288d
Don Restarone <35935196+donrestarone@users.noreply.github.com>
2022-12-03 14:45:01 -0500

    [feature] Ability to mention user in forum (#1256) (#1261)
    Addresses: #1173 
    
    Demo:
    
    https://user-images.githubusercontent.com/18470532/199933805-7669b727-cd76-43ad-9125-c8be379973a6.mov
    
    https://user-images.githubusercontent.com/18470532/200725154-1e9ce5f7-c7ed-4ec3-8b46-9c9bc232567f.mov
    
    
    Co-authored-by: Kripananda Yadav <70195786+kripananda-yadav@users.noreply.github.com>
    Co-authored-by: Ravi <rabigiri18@gmail.com>
    Co-authored-by: Regaron <Regaron@users.noreply.github.com>
    Co-authored-by: Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   include SimpleDiscussion::ForumUser
   include Comfy::Cms::WithCategories
+  include ActionText::Attachable
   # Include default devise modules. Others available are:
   #  and :omniauthable
   devise :invitable, :registerable,
