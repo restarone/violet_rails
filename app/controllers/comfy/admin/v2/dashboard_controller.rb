e89a928dea66343cfc6c9652eda9e32ae0916e67
Pralish Kayastha <pralishkayastha@gmail.com>
2022-12-26 21:18:35 +0545

    page visit graph

--- app/controllers/comfy/admin/v2/dashboard_controller.rb
+++ app/controllers/comfy/admin/v2/dashboard_controller.rb
@@ -6,6 +6,7 @@ class Comfy::Admin::V2::DashboardController < Comfy::Admin::Cms::BaseController
   def dashboard
     @page_visit_events_q = Ahoy::Event.where(name: 'comfy-cms-page-visit').joins(:visit).ransack(params[:q])
     @page_visit_events = @page_visit_events_q.result
+    @page_visit_events = @page_visit_events.jsonb_search(:properties, JSON.parse(params[:properties]).to_hash, params[:match]) if params[:properties]
     @page_visit_data = @page_visit_events.where.not('ahoy_visits.device_type': nil).group_by { |u| u.visit.device_type }.map do |key, value|
       { name: key, data: Ahoy::Event.where(id: value.pluck(:id)).group_by_week(:time).count }
     end
