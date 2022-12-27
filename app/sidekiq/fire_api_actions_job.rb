bbfa843d3a5a70dfa8406f4e82a375f80ac39994
Don Restarone <35935196+donrestarone@users.noreply.github.com>
2022-12-27 15:01:39 -0500

    [bug fix] Run Error API actions
    Addresses: https://github.com/restarone/violet_rails/issues/985
    
    Addresses: https://github.com/restarone/violet_rails/issues/1071
    
    ### Demo:
    
    https://user-images.githubusercontent.com/50227291/188673867-186d03e9-a8ec-4cb7-adb0-6a6483ba1cec.mov
    
    Since api resource is not saved due to any error, api action will keep track of user input with proper error message under 
    
    `additional_data['api_resource']['properties']` and `additional_data['api_resource']['errors']`
    
    Demo shows error message and user input being accessed by api_action
    
    ### Demo fix dead set functionality
    
    
    https://user-images.githubusercontent.com/50227291/189976658-e7c6fbe8-1f69-4740-9e98-98835a8fae26.mov
    
    ### Demo showing error message in API 
    https://user-images.githubusercontent.com/50227291/196223270-e1d831f3-8403-43eb-84c2-d5419ea078d4.mov
    
    Co-authored-by: Pralish Kayastha <50227291+Pralish@users.noreply.github.com>
    Co-authored-by: Pralish Kayastha <pralishkayastha@gmail.com>

--- app/sidekiq/fire_api_actions_job.rb
+++ app/sidekiq/fire_api_actions_job.rb
@@ -1,14 +1,26 @@
 class FireApiActionsJob
   include Sidekiq::Job
 
-  def perform(api_resource_id, action_class, current_user_id, current_visit_id, is_api_html_renderer_request)
+  # run error actions only after final retries
+  sidekiq_retries_exhausted do |msg, exception|
+    set_current_visit(msg['args'][1], msg['args'][2], msg['args'][3]) do
+      ApiAction.find(msg['args'][0]).execute_error_actions(exception.message)
+    end
+  end
+
+  def perform(action_id, current_user_id, current_visit_id, is_api_html_renderer_request)
+    FireApiActionsJob.set_current_visit(current_user_id, current_visit_id, is_api_html_renderer_request) do
+      api_action = ApiAction.find(action_id)
+      api_action.execute_action(false) if api_action.present?
+    end
+  end
+
+  def self.set_current_visit(current_user_id, current_visit_id, is_api_html_renderer_request)
     current_user = User.find_by(id: current_user_id)
     current_visit = Ahoy::Visit.find_by(id: current_visit_id)
 
     Current.set(user: current_user, visit: current_visit, is_api_html_renderer_request: is_api_html_renderer_request) do
-      api_resource = ApiResource.find(api_resource_id)
-  
-      api_resource.execute_model_context_api_actions(action_class)
+      yield
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 2022_11_28_055836) do
     t.text "method_definition", default: "raise StandardError"
     t.text "email_subject"
     t.integer "redirect_type", default: 0
+    t.jsonb "meta_data", default: {}
     t.index ["api_namespace_id"], name: "index_api_actions_on_api_namespace_id"
     t.index ["api_resource_id"], name: "index_api_actions_on_api_resource_id"
   end
