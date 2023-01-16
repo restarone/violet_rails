a19acb355cf139e915d2d482fce43f439d5054df
Pralish Kayastha <pralishkayastha@gmail.com>
2023-01-16 11:06:09 +0545

    revert migration

--- db/migrate/20230109150520_change_default_for_model_definition.rb
+++ db/migrate/20230109150520_change_default_for_model_definition.rb
@@ -1,15 +1,7 @@
 class ChangeDefaultForModelDefinition < ActiveRecord::Migration[6.1]
   OLD_MODEL_DEFINITION = "raise StandardError"
 
-  def up
-    change_column :external_api_clients, :model_definition, :text, default: ExternalApiClient::DEFAULT_MODEL_DEFINITION
-
-    ExternalApiClient.where(model_definition: OLD_MODEL_DEFINITION).update_all(model_definition: ExternalApiClient::DEFAULT_MODEL_DEFINITION)
-  end
-
-  def down
-    change_column :external_api_clients, :model_definition, :text, default: OLD_MODEL_DEFINITION
-
-    ExternalApiClient.where(model_definition: ExternalApiClient::DEFAULT_MODEL_DEFINITION).update_all(model_definition: OLD_MODEL_DEFINITION)
+  def change
+    change_column_default :external_api_clients, :model_definition, from: OLD_MODEL_DEFINITION, to: ExternalApiClient::DEFAULT_MODEL_DEFINITION
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_09_150520) do
+ActiveRecord::Schema.define(version: 2023_01_03_123121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -357,7 +357,7 @@ ActiveRecord::Schema.define(version: 2023_01_09_150520) do
     t.integer "retry_in_seconds", default: 0, null: false
     t.integer "max_retries", default: 1, null: false
     t.integer "retries", default: 0, null: false
-    t.text "model_definition", default: "class ExternalApiConnection\n  def initialize(parameters)\n    @external_api_client = parameters[:external_api_client]\n  end\n\n  def start\n    @external_api_client.api_namespace.api_resources.create(\n      properties: {\n        request_body: {}\n      }\n    )\n  end\nend\n\nExternalApiConnection"
+    t.text "model_definition", default: "raise StandardError"
     t.jsonb "state_metadata"
     t.jsonb "error_metadata"
     t.jsonb "metadata"
