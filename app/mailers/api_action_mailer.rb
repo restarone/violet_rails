f9e3577ff6c032acca68d8b9260d1dccb2dc5c12
Ajay Shrestha <er.ajaystha@gmail.com>
2023-01-17 15:35:29 -0500

    bugfix(): conditions update for fixes

--- app/mailers/api_action_mailer.rb
+++ app/mailers/api_action_mailer.rb
@@ -10,7 +10,7 @@ class ApiActionMailer < ApplicationMailer
       custom_subject = api_action.email_subject_evaluated || if api_action.api_namespace_action then  "#{api_action.type} #{api_action.api_namespace_action.api_namespace&.name.pluralize} v#{api_action.api_namespace_action.api_namespace&.version}" 
       else "#{api_action.type} #{@api_resource.api_namespace.name.pluralize} v#{@api_resource.api_namespace.version}" end
       
-        mail(
+      mail(
         from: from,
         to: mail_to,
         subject: custom_subject
