77e1236f388ce1b22980cfa671c5b162601cb8e7
Ajay Shrestha <er.ajaystha@gmail.com>
2023-01-20 23:36:35 -0500

    feat(): test cases update

--- app/mailers/api_action_mailer.rb
+++ app/mailers/api_action_mailer.rb
@@ -10,19 +10,8 @@ class ApiActionMailer < ApplicationMailer
       if !@api_resource && api_action.meta_data
        @meta_data =  api_action.meta_data["api_resource"]
        @meta_data["namespace"] = ApiNamespace.find_by(id: ApiAction.find(api_action.id).meta_data["api_resource"]["api_namespace_id"])
-      #  @meta_data["non_primitive_properties"] = [NonPrimitiveProperty.find_by(id: ApiAction.find(173).meta_data["api_resource"]["api_namespace_id"])]
-      #  @meta_data["non_primitive_properties"] = [NonPrimitiveProperty.find_by(id: ApiAction.find(173).meta_data["api_resource"]["api_namespace_id"])]
-        # @display_data = {
-        #   "api_namespace": {
-        #     "name": ApiNamespace.find_by(id: ApiAction.find(api_action.id).meta_data["api_resource"]["api_namespace_id"]),
-        #     "properties":  @api_action.meta_data["api_resource"]["properties"]
-        #   },
-        #   "properties":  @api_action.meta_data["api_resource"]["properties"]
-        # }
-        # @display_data =  { :api_namespace => {:name => ApiNamespace.find_by(id: ApiAction.find(api_action.id).meta_data["api_resource"]["api_namespace_id"]) }}
-        # @display_data[:api_namespace][:properties] = @api_action.meta_data["api_resource"]["properties"]
-        # @display_data["properties"] = @api_action.meta_data["api_resource"]["properties"]
       end
+      
       custom_subject = api_action.email_subject_evaluated || if api_action.api_namespace_action then  "#{api_action.type} #{api_action.api_namespace_action.api_namespace&.name.pluralize} v#{api_action.api_namespace_action.api_namespace&.version}" 
       else "#{api_action.type} #{@api_resource.api_namespace.name.pluralize} v#{@api_resource.api_namespace.version}" end
       
--- test/mailers/api_action_mailer_test.rb
+++ test/mailers/api_action_mailer_test.rb
@@ -1,27 +1,29 @@
 require "test_helper"
 
 class ApiActionMailerTest < ActionMailer::TestCase
-    test 'sends email wihout api resource if not included in api action' do
+    test 'sends email without api_resource if not included in api action' do
         @api_action_1 = api_actions(:error_api_action_one)
         @api_action_1.update(email_subject: "")
-        @api_action_2 = ErrorApiAction.create(@api_action_1.attributes.merge(custom_message: @api_action_1.custom_message.to_s, parent_id: @api_action_1.id, meta_data: { api_resource: {} }).except("id", "created_at", "updated_at", "api_namespace_id"))
+        @api_action_2 = ErrorApiAction.create(@api_action_1.attributes.merge(custom_message: @api_action_1.custom_message.to_s, parent_id: @api_action_1.id, meta_data: { api_resource: { errors: "Error Occured!", properties: {"Null"=>"", "Array"=>"", "Number"=>"", "Object"=>{"a"=>"b", "c"=>"d"}, "String"=>"", "Boolean"=>"false"} }, namespace: { name: "Namespace1" } }).except("id", "created_at", "updated_at", "api_namespace_id"))
 
         email = ApiActionMailer.send_email(@api_action_2)
         assert_emails 1 do
             email.deliver_later
         end
-        assert_equal "#{@api_action_2.type} #{@api_action_2.api_namespace_action&.api_namespace&.name.pluralize} v#{@api_action_2.api_namespace_action&.api_namespace&.version}",email.subject
+        refute_includes email.body, @api_action_2.meta_data["api_resource"]["properties"]
+        assert_equal "#{@api_action_2.type} #{@api_action_2.api_namespace_action&.api_namespace&.name.pluralize} v#{@api_action_2.api_namespace_action&.api_namespace&.version}", email.subject
     end
 
-    test 'sends email with api resource if included in api action' do
+    test 'sends email with api_resource if included in api action' do
         @api_action_1 = api_actions(:error_api_action_one_with_api_resource_data)
         @api_action_1.update(email_subject: "")
-        @api_action_2 = ErrorApiAction.create(@api_action_1.attributes.merge(custom_message: @api_action_1.custom_message.to_s, parent_id: @api_action_1.id, meta_data: { api_resource: {} }).except("id", "created_at", "updated_at", "api_namespace_id"))
+        @api_action_2 = ErrorApiAction.create(@api_action_1.attributes.merge(custom_message: @api_action_1.custom_message.to_s, parent_id: @api_action_1.id, meta_data: { api_resource: { errors: "Error Occured!", properties: {"Null"=>"", "Array"=>"", "Number"=>"", "Object"=>{"a"=>"b", "c"=>"d"}, "String"=>"", "Boolean"=>"false"} }, namespace: { name: "Namespace1" } }).except("id", "created_at", "updated_at", "api_namespace_id"))
 
         email = ApiActionMailer.send_email(@api_action_2)
         assert_emails 1 do
             email.deliver_later
         end
-        assert_equal "#{@api_action_2.type} #{@api_action_2.api_namespace_action&.api_namespace&.name.pluralize} v#{@api_action_2.api_namespace_action&.api_namespace&.version}",email.subject
+        assert_includes CGI.unescapeHTML(email.body.to_s), @api_action_2.meta_data["api_resource"]["properties"].to_s
+        assert_equal "#{@api_action_2.type} #{@api_action_2.api_namespace_action&.api_namespace&.name.pluralize} v#{@api_action_2.api_namespace_action&.api_namespace&.version}", email.subject
     end
 end
