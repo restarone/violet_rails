a23dde2e948d770d03e51ad9c44c91fa9c9d4e25
Don Restarone <35935196+donrestarone@users.noreply.github.com>
2023-01-25 16:18:54 -0500

    [bug fix] Dynamic links get broken in send_email
    Addresses: https://github.com/restarone/violet_rails/issues/1052
    
    Demo:
    
    
    https://user-images.githubusercontent.com/50227291/188492173-82f9a8ed-609a-44b1-a608-77aa0e1d1600.mov
    
    Co-authored-by: Pralish Kayastha <50227291+Pralish@users.noreply.github.com>
    Co-authored-by: Pralish Kayastha <pralishkayastha@gmail.com>

--- app/models/concerns/safe_executable_validator.rb
+++ app/models/concerns/safe_executable_validator.rb
@@ -28,8 +28,10 @@ class SafeExecutableValidator < ActiveModel::EachValidator
     SPLIT_DELIMITERS = ['(', ')', /\s/, '.', /\n/, /(?<!\:)\:(?!\:)/, '#{', '}', '=>', '"', '\'']
 
     def validate_each(record,attribute,value)
-        keywords = value.to_s.split(Regexp.union(SPLIT_DELIMITERS)).reject(&:blank?)
+        # decode and unescape string before checking for blacklisted kwywords
+        keywords = CGI.unescapeHTML(Addressable::URI.unencode(value.to_s)).split(Regexp.union(SPLIT_DELIMITERS)).reject(&:blank?)
         blacklisted_keywords_in_attribute = keywords & (BLACKLISTED_KEYWORDS - (options[:skip_keywords] || []))
+
         unless blacklisted_keywords_in_attribute.empty?
             record.errors.add(attribute, "contains disallowed keyword: #{blacklisted_keywords_in_attribute.to_s}. Please refactor #{attribute} accordingly")
         end
