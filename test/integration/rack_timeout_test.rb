4d90f5f38533df962da20b7beb60449690705f84
Pralish Kayastha <pralishkayastha@gmail.com>
2023-02-17 07:45:15 +0545

    reduce sleep time

--- test/integration/rack_timeout_test.rb
+++ test/integration/rack_timeout_test.rb
@@ -5,21 +5,21 @@ class RackTimeoutTest < ActionDispatch::IntegrationTest
     # create fake routes to mock long processing time
     Rails.application.routes.draw do
       get '/sleep', to: -> (env) { 
-        sleep(2) 
+        sleep(0.5) 
         [200, {}, ['Hello, world!']]
       }
     end
   end
 
   test "should interrupt request when timeout is exceeded" do
-    Rack::Timeout.any_instance.stubs(:service_timeout).returns(1)
+    Rack::Timeout.any_instance.stubs(:service_timeout).returns(0.1)
     assert_raises(Rack::Timeout::RequestTimeoutError) do
       get "/sleep"
     end
   end
 
   test "should not interrupt request when timeout is not exceeded" do
-    Rack::Timeout.any_instance.stubs(:service_timeout).returns(3)
+    Rack::Timeout.any_instance.stubs(:service_timeout).returns(1)
     get "/sleep"
     assert_response :success
   end
