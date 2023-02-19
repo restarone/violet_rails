a16111b225b56792a90975c46d1c736b250fb8a8
Ajay Shrestha <er.ajaystha@gmail.com>
2023-02-19 14:15:08 -0500

    configuring middleware for safe exception

--- test/integration/rack_timeout_test.rb
+++ test/integration/rack_timeout_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class RackTimeoutTest < ActionDispatch::IntegrationTest
   setup do
+    # setup_mock_objects
     setup_fake_routes
   end
 
@@ -18,6 +19,38 @@ class RackTimeoutTest < ActionDispatch::IntegrationTest
       get '/root', to: -> (env) { 
         [200, {}, ['Hello from root page!']]
       }
+    end
+  end
+
+  def setup_mock_objects
+    # Mock MyModel
+    MyModel.any_instance.stubs(:foo).returns("bar")
+
+    # Mock MyBackgroundJob
+    MyBackgroundJob.stubs(:perform_now).returns(true)
+  end
+
+  # class MyBackgroundJob
+  #   def perform
+  #     sleep(2)
+  #     MyModel.create(name: "test")
+  #   end
+  # end
+
+  # class MyModel < ApplicationRecord
+  #   validates :name, presence: true
+  # end
+
+  class MyModel
+    def foo
+      "bar"
+    end
+  end
+
+  class MyBackgroundJob < ApplicationJob
+    def perform
+      sleep(2)
+      MyModel.new.foo
     end
   end
 
@@ -45,75 +78,19 @@ class RackTimeoutTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should handle web requests and background jobs" do
-  #   # Set the timeout to 1 second
-  #   Rack::Timeout.any_instance.stubs(:service_timeout).returns(1)
-  #   my_background_job_mock = mock('MyBackgroundJob')
-  #   MyBackgroundJob.expects(:new).returns(my_background_job_mock)
+  test "should run other requests and background job should run even after request timeout exception" do
+    Rack::Timeout.any_instance.stubs(:service_timeout).returns(0.1)
+    MyBackgroundJob.perform_now
+    assert_raises(Rack::Timeout::RequestTimeoutError) do
+      get "/sleep"
+    end
 
-  #   # Make a request to a healthy endpoint to ensure the server is responsive
-  #   get '/root'
-  #   assert_response :success
+    # Assert that a request to a healthy endpoint succeeds
+    get '/root'
+    assert_response :success
 
-  #   my_background_job_mock.expects(:perform)
+    # Assert that the background job succeeds after request exception
+    assert_equal "bar", MyBackgroundJob.new.perform
+  end
 
-  #   # Create and perform the background job
-  #   job = MyBackgroundJob.new
-  #   job.perform
-  # end
-
-  # class MyBackgroundJob < ApplicationJob
-  #   queue_as :default
-
-  #   def perform
-  #     # my_model = MyModel.find(1)
-  #     # sleep 0.5
-  #     # my_model.update(status: "completed")
-  #   end
-  # end
-
-  # test "should handle web requests and background jobs after request timeout exception" do
-  #   Rack::Timeout.any_instance.stubs(:service_timeout).returns(0.1)
-  #   assert_raises(Rack::Timeout::RequestTimeoutError) do
-  #     get "/sleep"
-  #   end
-  
-  #   # Assert that a request to a healthy endpoint succeeds
-  #   get '/root'
-  #   assert_response :success
-  
-  #   # Mock a background job that takes longer than the service timeout
-  #   my_job = MyBackgroundJob1.new
-  #   MyBackgroundJob1.stubs(:new).returns(my_job)
-  #   my_job.stubs(:perform).raises(Rack::Timeout::RequestTimeoutError)
-  
-  #   # Assert that the background job raises a RequestTimeoutError
-  #   assert_raises(Rack::Timeout::RequestTimeoutError) do
-  #     my_job.perform_now
-  #   end
-  
-  #   # Assert that a request to a healthy endpoint still succeeds
-  #   get '/root'
-  #   assert_response :success
-  
-  #   # Replace the mocked MyBackgroundJob1 with the original implementation
-  #   MyBackgroundJob1.unstub(:new)
-  #   my_job.unstub(:perform)
-  # end
-
-  # class MyBackgroundJob1
-  #   def perform_now
-  #     # Do some background work here
-  #     # ...
-  #     # Update a model
-  #     my_model = MyModel.find(1)
-  #     my_model.update(name: "New name")
-  #   end
-
-  #   def perform
-  #     # simulate some background job work
-  #     sleep(1)
-  #     MyModel.create(name: "Background Job 1")
-  #   end
-  # end
 end
