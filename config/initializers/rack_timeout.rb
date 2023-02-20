e46b2961d9b0f30ea246d664e446774fbfea519b
Er. Ajay Shrestha <55496123+ajaystha42@users.noreply.github.com>
2023-02-20 15:36:00 -0500

    [feature] rack-timeout for limiting request time
    Addresses: https://github.com/restarone/violet_rails/issues/1397
    
    Demo: 
    
    https://user-images.githubusercontent.com/55496123/217051621-69b8e185-31e3-4288-b533-383046e7fd2e.mov
    
    Co-authored-by: Pralish Kayastha <pralishkayastha@gmail.com>

---
+++ config/initializers/rack_timeout.rb
@@ -0,0 +1,27 @@
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout
+
+module MyRackTimeout
+  module Timeout
+    class CustomTimeoutMiddleware
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+        service_timeout = env['RACK_TIMEOUT_SERVICE_TIMEOUT'].to_f
+        thread = Thread.current
+        timer_thread = Thread.new do
+          sleep service_timeout
+          thread.safe_raise(timeout: 3, exception: "Request timed out")
+        end
+
+        status, headers, body = @app.call(env)
+        [status, headers, body]
+      ensure
+        timer_thread.kill if timer_thread&.alive?
+      end
+    end
+  end
+end
+
+Rails.application.config.middleware.insert_before Rack::Timeout, MyRackTimeout::Timeout::CustomTimeoutMiddleware
---
+++ test/integration/rack_timeout_test.rb
@@ -0,0 +1,84 @@
+require "test_helper"
+
+class RackTimeoutTest < ActionDispatch::IntegrationTest
+  setup do
+    setup_fake_routes
+  end
+
+  teardown do
+    Rails.application.reload_routes!
+  end
+
+  def setup_fake_routes
+    Rails.application.routes.draw do
+      get '/sleep', to: -> (env) { 
+        sleep(0.5) 
+        [200, {}, ['Hello, world!']]
+      }
+      get '/root', to: -> (env) { 
+        [200, {}, ['Hello from root page!']]
+      }
+    end
+  end
+
+  def setup_mock_objects
+    # Mock MyModel
+    MyModel.any_instance.stubs(:foo).returns("bar")
+
+    # Mock MyBackgroundJob
+    MyBackgroundJob.stubs(:perform_now).returns(true)
+  end
+
+  class MyModel
+    def foo
+      "bar"
+    end
+  end
+
+  class MyBackgroundJob < ApplicationJob
+    def perform
+      sleep(2)
+      MyModel.new.foo
+    end
+  end
+
+  test "should interrupt request when timeout is exceeded" do
+    Rack::Timeout.any_instance.stubs(:service_timeout).returns(0.1)
+    assert_raises(Rack::Timeout::RequestTimeoutError) do
+      get "/sleep"
+    end
+  end
+
+  test "should not interrupt request when timeout is not exceeded" do
+    Rack::Timeout.any_instance.stubs(:service_timeout).returns(1)
+    get "/sleep"
+    assert_response :success
+  end
+
+  test "should interrupt request and check server health" do
+    Rack::Timeout.any_instance.stubs(:service_timeout).returns(0.1)
+    assert_raises(Rack::Timeout::RequestTimeoutError) do
+      get "/sleep"
+    end
+
+    # Assert that a request to a healthy endpoint succeeds
+    get '/root'
+    assert_response :success
+  end
+
+  test "should run other requests and background job should run even after request timeout exception" do
+    Rack::Timeout.any_instance.stubs(:service_timeout).returns(0.1)
+    MyBackgroundJob.perform_now
+    assert_raises(Rack::Timeout::RequestTimeoutError) do
+      get "/sleep"
+    end
+
+    # Assert that a request to a healthy endpoint succeeds
+    get '/root'
+    assert_response :success
+
+    # Assert that the background job succeeds after request exception
+    assert_equal "bar", MyBackgroundJob.new.perform
+  end
+
+end
