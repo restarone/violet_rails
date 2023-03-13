0269e9c8b47fa23404e272c8da38bf2be0e65a03
Pralish Kayastha <pralishkayastha@gmail.com>
2023-03-13 15:34:46 +0545

    remove rack-timeout and integrate slowpole

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,4 +129,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.slowpoke.timeout = ENV['VIOLET_SERVICE_TIMEOUT'].to_i.nonzero? || 15
 end
