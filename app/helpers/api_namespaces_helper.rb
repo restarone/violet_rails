4cd15db509c486c34ca92deefdfeed2cb0f293ec
Don Restarone <35935196+donrestarone@users.noreply.github.com>
2023-03-15 15:31:19 -0400

    [feature] Ability to query API resources with KEYWORDS
    Addresses: https://github.com/restarone/violet_rails/issues/1374
    
    **DEMO**
    
    https://user-images.githubusercontent.com/25191509/214291392-23e3c962-31ce-4df3-b3fa-1a2565c978be.mov
    
    ### **For client-engineers**
    For http-request to search by `KEYWORDS`, we would need to pass `option:  KEYWORDS` as key-value pair for the attributes we want to search by in `properties` payload.
    
    In the clip below, we are filtering `cars` by searching in its attributes, namely: `make`, `model` and `type` with `KEYWORDS` option.
    
    https://user-images.githubusercontent.com/25191509/224547568-37ff1e9c-51e0-4212-bfa4-dd123c9857ac.mov
    
    Payload Example:
    ```
    properties: {"make":{"value":"kia tesla","option":"KEYWORDS"},"model":{"value":"kia tesla","option":"KEYWORDS"},"type":{"value":"kia tesla","option":"KEYWORDS"}},
    match: ANY
    ```
    Co-authored-by: Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>

--- app/helpers/api_namespaces_helper.rb
+++ app/helpers/api_namespaces_helper.rb
@@ -10,4 +10,27 @@ module ApiNamespacesHelper
   def system_paths
     Comfy::Cms::Page.all.pluck(:full_path)
   end
+
+  def api_html_renderer_dynamic_properties(namespace, search_option = nil)
+    custom_properties = {}
+    fields_in_properties = namespace.properties.keys
+
+    namespace.properties.values.each_with_index do |obj,index|
+      if obj.present? && obj != "nil" && obj != "\"\""
+        if search_option.present?
+          next if !obj.is_a?(Array) && !obj.is_a?(String)
+
+          custom_properties[fields_in_properties[index]] = {
+            value: obj.is_a?(Array) ? obj.first(1) : obj.split.first,
+            option: search_option
+          }
+        else
+          custom_properties[fields_in_properties[index]] = obj;
+        end
+      end
+    end
+
+    # sanitize the text to properly display
+    JSON.parse(custom_properties.to_json, object_class: OpenStruct).to_s.gsub(/=/,': ').gsub(/#<OpenStruct/,'{').gsub(/>/,' }').gsub("\\", "'").gsub(/"'"/,'"').gsub(/'""/,'"')
+  end
 end
--- app/models/concerns/jsonb_search/query_builder.rb
+++ app/models/concerns/jsonb_search/query_builder.rb
@@ -2,13 +2,19 @@ module JsonbSearch
   module QueryBuilder
     QUERY_OPTION = {
       EXACT: 'EXACT',
-      PARTIAL: 'PARTIAL'
+      PARTIAL: 'PARTIAL',
+      KEYWORDS: 'KEYWORDS'
     }.freeze
 
     MATCH_OPTION = {
       ALL: 'ALL',
       ANY: 'ANY'
-  }.freeze
+    }.freeze
+
+    # https://github.com/Altoros/belarus-ruby-on-rails/blob/master/solr/conf/stopwords.txt
+    STOP_WORDS = [
+      'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 'for', 'if', 'in', 'into', 'is', 'it', 'no', 'not', 'of', 'on', 'or', 's', 'such', 't', 'that', 'the', 'their', 'then', 'there', 'these', 'they', 'this', 'to', 'was', 'will', 'with'
+    ]
 
     class << self
       def build_jsonb_query(column_name, query_params, match = nil)
@@ -23,12 +29,15 @@ module JsonbSearch
 
         query_params.each do |key, value|
           if value.is_a?(Hash) && value.key?(:value)
+            next unless value[:value].present?
             # { name: { value: 'violet', query }} || { name: { value: 'violet', option: 'EXACT' }}
             queries << { option: value[:option] || QUERY_OPTION[:EXACT], key: key, value: value[:value], match: value[:match] }
           elsif value.is_a?(Hash)
             # { foo: { bar: 'baz', wat: 'up' }}
             value.each do |k, v|
               if v.is_a?(Hash) && v.key?(:value)
+                next unless v[:value].present?
+
                 queries <<  { key: key, value: [{ option: v[:option] || QUERY_OPTION[:EXACT], key: k, value: v[:value], match: v[:match] }] }
               else
                 queries << { key: key, value: [{ option: QUERY_OPTION[:EXACT], key: k, value: v }]}
@@ -75,12 +84,33 @@ module JsonbSearch
         end
       end
 
-      # "column ->> 'property' = 'term'" 
+      # "column ->> 'property' = 'term'"
       def string_query(key, term, option, query)
-        if option == QUERY_OPTION[:PARTIAL]
+        if option == QUERY_OPTION[:KEYWORDS]
+          query_array = []
+          operator = 'LIKE'
+
+          terms = term.split(' ') - STOP_WORDS
+          terms = terms.map { |txt| "%#{txt}%" }
+
+          terms.each do |txt|
+            query_array << generate_string_sql(query, key, txt, operator)
+          end
+
+          query_array << generate_string_sql(query, key, "%#{term}%", operator) if terms.size > 1
+
+          query_array.join(' OR ')
+        elsif option == QUERY_OPTION[:PARTIAL]
           term = "%#{term}%"
           operator = 'LIKE'
+
+          generate_string_sql(query, key, term, operator)
+        else
+          generate_string_sql(query, key, term)
         end
+      end
+
+      def generate_string_sql(query, key, term, operator = nil)
         # A ' inside a string quoted with ' may be written as ''.
         # https://stackoverflow.com/questions/54144340/how-to-query-jsonb-fields-and-values-containing-single-quote-in-rails#comment95120456_54144340
         # https://dev.mysql.com/doc/refman/8.0/en/string-literals.html#character-escape-sequences
@@ -95,7 +125,21 @@ module JsonbSearch
 
       # "column -> 'property' ? '['term']'" 
       def array_query(key, term, option, query, match)
-        if option == QUERY_OPTION[:PARTIAL]
+        if option == QUERY_OPTION[:KEYWORDS]
+          query_array = []
+          operator = 'LIKE'
+
+          term.each do |data|
+            items = data.split(' ') - STOP_WORDS
+            items = items.map { |txt| "%#{txt}%" }
+
+            items.each do |item|
+              query_array  << "lower(#{query} ->> '#{key}'::text) LIKE lower('#{item.to_s.gsub("'", "''")}')"
+            end
+          end
+
+          query_array.join(match == MATCH_OPTION[:ANY] ? ' OR ' : ' AND ')
+        elsif option == QUERY_OPTION[:PARTIAL]
           match == MATCH_OPTION[:ANY] ? term.map { |q| "#{query} -> '#{key}' ? '#{q}'" }.join(' OR ') : "#{query} -> '#{key}' @> '#{term.to_json.gsub("'", "''")}'"
         else
           "#{query} -> '#{key}' @> '#{term.to_json}' AND #{query} -> '#{key}' <@ '#{term.to_json.gsub("'", "''")}'"
