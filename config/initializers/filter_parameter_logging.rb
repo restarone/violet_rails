15762f6b89690af0cc4700dfa1d078830d5c2633
donrestarone <shashikejayatunge@gmail.co>
2023-03-18 12:54:31 -0400

    scrub logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,5 +2,5 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :email, :otp_secret, :encrypted_otp_secret_salt, :encrypted_otp_secret_iv, :"warden.user.user.key", :"_csrf_token"
 ]
