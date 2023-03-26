fbcc7ac0f4e32ae7fa6096cb83893d53169d00a3
Pralish Kayastha <pralishkayastha@gmail.com>
2023-03-26 21:26:26 +0545

    create temp prorperties and backfill

--- db/migrate/20230326151717_add_temp_properties_to_api_namespaces_and_backfill.rb
+++ db/migrate/20230326151717_add_temp_properties_to_api_namespaces_and_backfill.rb
@@ -2,9 +2,7 @@ class AddTempPropertiesToApiNamespacesAndBackfill < ActiveRecord::Migration[6.1]
   def up
     add_column :api_namespaces, :temp_properties, :json
     
-    ApiNamespace.where(temp_properties: nil).in_batches do |records|
-      records.update_all("temp_properties = properties")
-    end
+    ApiNamespace.where(temp_properties: nil).update_all("temp_properties = properties")
   end
 
   def down
