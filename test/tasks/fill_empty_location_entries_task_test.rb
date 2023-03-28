7f9a6ed3eaac63691f1d6e59564b916760be7ba0
Lascau Sebastian <lascauionutsebastian98@gmail.com>
2023-03-28 14:12:53 +0300

    fixing ahoy rake task test v2

--- test/tasks/fill_empty_location_entries_task_test.rb
+++ test/tasks/fill_empty_location_entries_task_test.rb
@@ -13,9 +13,16 @@ class FillEmptyLocationEntriesTaks < ActiveSupport::TestCase
       user_agent: Faker::Internet.user_agent, 
       landing_page: 'http://localhost:5250/'
     )
+    Rails.application.load_tasks if Rake::Task.tasks.empty?
   end
-
+  
   test 'populate location of a new created entry' do
+    Ahoy::Visit.any_instance.stubs(:ip).returns(@new_visit.ip)
+    pp @new_visit
+    assert_equal @new_visit.country.nil?, true
+    assert_equal @new_visit.region.nil?, true
+    assert_equal @new_visit.city.nil?, true
+
     Rake::Task["ahoy:fill_empty_location_entries"].invoke
 
     assert_equal @new_visit.country.nil?, false
