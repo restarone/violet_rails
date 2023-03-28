00fa9b92b290400fc5851c7409e6da30401662a5
Lascau Sebastian <lascauionutsebastian98@gmail.com>
2023-03-28 10:33:45 +0300

    variable new_visit small fix

--- test/tasks/fill_empty_location_entries_task_test.rb
+++ test/tasks/fill_empty_location_entries_task_test.rb
@@ -4,7 +4,7 @@ require 'faker'
 
 class FillEmptyLocationEntriesTaks < ActiveSupport::TestCase
   setup do
-    new_visit = Ahoy::Visit.create!(
+    @new_visit = Ahoy::Visit.create!(
       started_at: Time.now,
       ip: Faker::Internet.ip_v4_address, 
       os: 'GNU/Linux', 
@@ -18,8 +18,8 @@ class FillEmptyLocationEntriesTaks < ActiveSupport::TestCase
   test 'populate location of a new created entry' do
     Rake::Task["ahoy:fill_empty_location_entries"].invoke
 
-    assert_equal new_visit.country.nil?, false
-    assert_equal new_visit.region.nil?, false
-    assert_equal new_visit.city.nil?, false
+    assert_equal @new_visit.country.nil?, false
+    assert_equal @new_visit.region.nil?, false
+    assert_equal @new_visit.city.nil?, false
   end
 end
