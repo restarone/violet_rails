015669704116a172877d71ac50f143b21f773431
Pralish Kayastha <pralishkayastha@gmail.com>
2023-03-28 21:01:13 +0545

    refactor code

--- test/tasks/fill_empty_location_entries_task_test.rb
+++ test/tasks/fill_empty_location_entries_task_test.rb
@@ -11,28 +11,39 @@ class FillEmptyLocationEntriesTaks < ActiveSupport::TestCase
       browser: 'Firefox', 
       device_type: 'Desktop', 
       user_agent: Faker::Internet.user_agent, 
-      landing_page: 'http://localhost:5250/'
+      landing_page: 'http://localhost:5250/',
+      visit_token: 'random_hash'
     )
     Rails.application.load_tasks if Rake::Task.tasks.empty?
     Sidekiq::Testing.fake!
   end
   
   test 'populate location of a new created entry' do
+    stub_request(:get, /http:\/\/ipinfo.io\/*/).to_return(status: 200, body: {
+        "ip": "110.41.52.64",
+        "hostname": "ecs-110-41-52-64.compute.hwclouds-dns.com",
+        "city": "Guangzhou",
+        "region": "Guangdong",
+        "country": "CN",
+        "loc": "23.1167,113.2500",
+        "org": "AS55990 Huawei Cloud Service data center",
+        "timezone": "Asia/Shanghai",
+        "readme": "https://ipinfo.io/missingauth"
+    }.to_json)
 
-    assert_equal @new_visit.country.nil?, true
-    assert_equal @new_visit.region.nil?, true
-    assert_equal @new_visit.city.nil?, true
+    refute @new_visit.country
+    refute @new_visit.region
+    refute @new_visit.city
 
     perform_enqueued_jobs do
       Rake::Task["ahoy:fill_empty_location_entries"].invoke
       Sidekiq::Worker.drain_all
     end
 
-    sleep 2
-
     @new_visit.reload
-    assert_equal @new_visit.country.nil?, false
-    assert_equal @new_visit.region.nil?, false
-    assert_equal @new_visit.city.nil?, false
+
+    assert_equal 'CN', @new_visit.country
+    assert_equal 'Guangdong', @new_visit.region
+    assert_equal 'Guangzhou', @new_visit.city
   end
 end
