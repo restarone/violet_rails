7f8ee2f8ca796da5a224bfa5a8b11ec53f2638aa
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-04-07 22:14:59 +0545

    feat(): fix strong_migration error

--- db/migrate/20230321055733_add_index_to_ahoy_events_name_and_time.rb
+++ db/migrate/20230321055733_add_index_to_ahoy_events_name_and_time.rb
@@ -1,6 +1,8 @@
 class AddIndexToAhoyEventsNameAndTime < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
   def change
-    add_index :ahoy_events, :name
-    add_index :ahoy_events, :time
+    add_index :ahoy_events, :name, algorithm: :concurrently
+    add_index :ahoy_events, :time, algorithm: :concurrently
   end
 end
