865de69d6127b52b4bff3ab5f7cad5c183a51751
Pralish Kayastha <pralishkayastha@gmail.com>
2023-04-07 17:14:37 +0545

    change strong migration date

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,5 +1,5 @@
 # Mark existing migrations as safe
-StrongMigrations.start_after = 20230404085503
+StrongMigrations.start_after = 20230310222034
 
 # Set timeouts for migrations
 # If you use PgBouncer in transaction mode, delete these lines and set timeouts on the database user
