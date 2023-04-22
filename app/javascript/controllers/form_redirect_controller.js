b679a471813209cb3904b2abb1b9e698aefe7f7b
Pralish Kayastha <pralishkayastha@gmail.com>
2023-04-22 21:55:21 +0545

    redirect from form

---
+++ app/javascript/controllers/form_redirect_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+import * as Turbo from "@hotwired/turbo"
+
+export default class extends Controller {
+  next(event) {
+    console.log(event)
+    if (event.detail.success) {
+      const fetchResponse = event.detail.fetchResponse
+
+      // history.pushState(
+      //   { turbo_frame_history: true },
+      //   "",
+      //   fetchResponse.response.url
+      // )
+
+      // Turbo.visit(fetchResponse.response.url)
+    }
+  }
+}
