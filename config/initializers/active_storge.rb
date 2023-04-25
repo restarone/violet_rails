7babf63789a61d82f58971696828205918457155
Pralish Kayastha <pralishkayastha@gmail.com>
2023-04-25 23:40:42 +0545

    rotate keys for active storage verigier

--- config/initializers/active_storge.rb
+++ config/initializers/active_storge.rb
@@ -12,3 +12,12 @@ if Rails.env == 'production' || Rails.env == 'test'
     end
   end
 end
+
+if ENV['OLD_SECRET_KEY_BASE'].present?
+  Rails.application.config.after_initialize do |app|
+    key_generator =  ActiveSupport::KeyGenerator.new(ENV['OLD_SECRET_KEY_BASE'], iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1)
+    secret = key_generator.generate_key("ActiveStorage")
+    app.message_verifier("ActiveStorage").rotate(secret)
+  end
+end
+
--- config/initializers/cookie_rotator.rb
+++ config/initializers/cookie_rotator.rb
@@ -1,19 +1,21 @@
-Rails.application.config.after_initialize do
-  Rails.application.config.action_dispatch.cookies_rotations.tap do |cookies|
-    authenticated_encrypted_cookie_salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
-    signed_cookie_salt = Rails.application.config.action_dispatch.signed_cookie_salt
+if ENV['OLD_SECRET_KEY_BASE'].present?
+  Rails.application.config.after_initialize do |app|   
+    Rails.application.config.action_dispatch.cookies_rotations.tap do |cookies|
+      authenticated_encrypted_cookie_salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
+      signed_cookie_salt = Rails.application.config.action_dispatch.signed_cookie_salt
 
-    secret_key_base = ENV['OLD_SECRET_KEY_BASE']
+      secret_key_base = ENV['OLD_SECRET_KEY_BASE']
 
-    key_generator = ActiveSupport::KeyGenerator.new(
-      secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
-    )
-    key_len = ActiveSupport::MessageEncryptor.key_len
+      key_generator = ActiveSupport::KeyGenerator.new(
+        secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
+      )
+      key_len = ActiveSupport::MessageEncryptor.key_len
 
-    old_encrypted_secret = key_generator.generate_key(authenticated_encrypted_cookie_salt, key_len)
-    old_signed_secret = key_generator.generate_key(signed_cookie_salt)
+      old_encrypted_secret = key_generator.generate_key(authenticated_encrypted_cookie_salt, key_len)
+      old_signed_secret = key_generator.generate_key(signed_cookie_salt)
 
-    cookies.rotate :encrypted, old_encrypted_secret
-    cookies.rotate :signed, old_signed_secret
+      cookies.rotate :encrypted, old_encrypted_secret
+      cookies.rotate :signed, old_signed_secret
+    end
   end
 end
