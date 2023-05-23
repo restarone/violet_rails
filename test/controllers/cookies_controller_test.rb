ecaf04dca50fff607d0e53ac06833536c9146667
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-05-23 13:43:30 +0545

    feat(): add geolocation data in cookies/fetch and add test cases

--- test/controllers/cookies_controller_test.rb
+++ test/controllers/cookies_controller_test.rb
@@ -50,7 +50,7 @@ class CookiesControllerTest < ActionDispatch::IntegrationTest
     get cookies_fetch_url
 
     assert_response :success
-    assert_equal ['cookies_accepted', 'ahoy_visitor_token', 'ahoy_visit_token'].sort, response.parsed_body.keys.sort
+    assert_equal ['cookies_accepted', 'ahoy_visitor_token', 'ahoy_visit_token', 'metadata'].sort, response.parsed_body.keys.sort
   end
 
   test 'should not return cookie information if the cookie consent information was rejected.' do
@@ -60,7 +60,65 @@ class CookiesControllerTest < ActionDispatch::IntegrationTest
     get cookies_fetch_url
 
     assert_response :success
-    assert_equal ['message'], response.parsed_body.keys
+    assert_equal ['message', 'metadata'], response.parsed_body.keys
     assert_equal 'Cookies were rejected or has not been accepted.', response.parsed_body['message']
+  end
+
+  test 'should return geolocation data if cookie consent was accepted' do
+    WebMock.allow_net_connect!(net_http_connect_on_start: true)
+    self.remote_addr = '110.33.122.75' # random IP of Australia
+    
+    @subdomain.update(tracking_enabled: true)
+
+    get cookies_url, params: { cookies: true}
+    get cookies_fetch_url
+
+    assert_response :success
+    assert_equal ['cookies_accepted', 'ahoy_visitor_token', 'ahoy_visit_token', 'metadata'].sort, response.parsed_body.keys.sort
+    
+    metadata = response.parsed_body['metadata']
+    assert metadata
+    assert_equal '110.33.122.75', metadata['ip_address']
+    assert_equal request.safe_location.country, metadata['country']
+    assert_equal request.safe_location.country_code, metadata['country_code']
+  end
+
+  test 'should return geolocation data even if cookie consent was rejected' do
+    WebMock.allow_net_connect!(net_http_connect_on_start: true)
+    self.remote_addr = '110.33.122.75' # random IP of Australia
+    
+    @subdomain.update(tracking_enabled: true)
+
+    get cookies_url, params: { cookies: false}
+
+    get cookies_fetch_url
+
+    assert_response :success
+    assert_equal 'Cookies were rejected or has not been accepted.', response.parsed_body['message']
+    
+    metadata = response.parsed_body['metadata']
+    assert metadata
+    assert_equal '110.33.122.75', metadata['ip_address']
+    assert_equal request.safe_location.country, metadata['country']
+    assert_equal request.safe_location.country_code, metadata['country_code']
+  end
+
+  test 'should return geolocation data accordingly when the country location details cannot be determined from IP' do
+    WebMock.allow_net_connect!(net_http_connect_on_start: true)
+    self.remote_addr = '127.0.0.1' # localhost IP
+    
+    @subdomain.update(tracking_enabled: true)
+
+    get cookies_url, params: { cookies: true}
+    get cookies_fetch_url
+
+    assert_response :success
+    assert_equal ['cookies_accepted', 'ahoy_visitor_token', 'ahoy_visit_token', 'metadata'].sort, response.parsed_body.keys.sort
+    
+    metadata = response.parsed_body['metadata']
+    assert metadata
+    assert_equal '127.0.0.1', metadata['ip_address']
+    assert_equal 'not available', metadata['country']
+    assert_equal 'not available', metadata['country_code']
   end
 end
