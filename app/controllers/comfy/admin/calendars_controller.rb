8ad110ec0e21e3afc6679561ed2c2fcbf25218b0
donrestarone <shashikejayatunge@gmail.com>
2023-09-13 12:29:23 -0400

    query and show meetings

--- app/controllers/comfy/admin/calendars_controller.rb
+++ app/controllers/comfy/admin/calendars_controller.rb
@@ -5,6 +5,8 @@ class Comfy::Admin::CalendarsController < Comfy::Admin::Cms::BaseController
   end
 
   def index
-
+  # Scope your query to the dates being shown:
+  start_date = params.fetch(:start_date, Date.today).to_date
+  @meetings = Meeting.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,3 @@
 class Meeting < ApplicationRecord
+
 end
