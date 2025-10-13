815d1203f40010db4bb92e5d8b1764b6d19e315f
Restarone Solutions <restarone@e6:17:9a:35:dd:4b.home>
2025-10-13 15:31:34 -0400

    include html title as the emailer name

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,4 +1,4 @@
 class DeviseMailer < Devise::Mailer
-  default from: "#{Subdomain.current.name} #{Subdomain.current.html_title} <#{Subdomain.current.name}@#{ENV['APP_HOST']}>"
+  default from: "#{Subdomain.current.html_title} <#{Subdomain.current.name}@#{ENV['APP_HOST']}>"
   default "Message-ID" => lambda {"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{ENV['APP_HOST']}"}
 end
