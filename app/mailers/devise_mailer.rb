569adc1c9998aea580b8a88c4ddd40833b25ae77
Restarone Solutions <restarone@e6:17:9a:35:dd:4b.home>
2025-10-13 13:05:44 -0400

    set sender in devise mailer

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,3 +1,5 @@
 class DeviseMailer < Devise::Mailer
   default "Message-ID" => lambda {"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{ENV['APP_HOST']}"}
+  default "from" => lambda {"#{Subdomain.current.name}@#{ENV['APP_HOST']}"}
+  default "reply_to" => lambda {"#{Subdomain.current.name}@#{ENV['APP_HOST']}"}
 end
