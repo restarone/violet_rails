ca020ab4ac36f48559b206e47a8665b1f9df1c96
Restarone Solutions Engineering <101986515+restarone-solutions@users.noreply.github.com>
2025-10-13 16:18:12 -0400

    set sender in devise mailer (#1680)
    * set sender in devise mailer
    
    * dont set mailer sender at boot time
    
    * pass custom mailer to be used by user model
    
    * include html title as the emailer name
    
    ---------
    
    Co-authored-by: Restarone Solutions <restarone@e6:17:9a:35:dd:4b.home>

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,3 +1,4 @@
 class DeviseMailer < Devise::Mailer
+  default from: "#{Subdomain.current.html_title} <#{Subdomain.current.name}@#{ENV['APP_HOST']}>"
   default "Message-ID" => lambda {"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{ENV['APP_HOST']}"}
 end
