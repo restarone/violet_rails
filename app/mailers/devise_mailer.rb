3c550fa74702659844ed0d5a8fad06703d74b0ae
Restarone Solutions <restarone@Restarones-MacBook-Pro.local>
2025-10-16 12:33:51 -0400

    set safe default at boot time, and remove default sender from mailer class and add example for customizing devise mailer

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,4 +1,20 @@
 class DeviseMailer < Devise::Mailer
-  default from: "#{Subdomain.current.html_title} <#{Subdomain.current.name}@#{ENV['APP_HOST']}>"
   default "Message-ID" => lambda {"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{ENV['APP_HOST']}"}
+
+  def invitation_instructions(record, token, opts = {})
+    @resource = record
+    @token = token
+
+    mail_settings = {
+      to: @resource.email, 
+      subject: "Your invitation to #{@resource.subdomain}",
+      from: @from,
+    }
+
+    mail(mail_settings) do |format|
+      format.text { render plain: render_to_string(template: "users/mailer/invitation_instructions") }
+      format.html { render html: render_to_string(template: "users/mailer/invitation_instructions").html_safe }
+    end
+  end
+  
 end
