f624a63fd783b41d91e9fa562b8d5b58faeb4362
Restarone Solutions Inc. Software Engineering <engineering@restarone.solutions>
2025-10-18 10:53:49 -0400

    remove unused import

--- app/controllers/comfy/admin/sales_collateral_controller.rb
+++ app/controllers/comfy/admin/sales_collateral_controller.rb
@@ -1,9 +1,6 @@
 class Comfy::Admin::SalesCollateralController < Comfy::Admin::Cms::BaseController
-  include DashboardHelper
-
   before_action :ensure_authority_to_manage_analytics
 
   def dashboard
-    
   end
 end
