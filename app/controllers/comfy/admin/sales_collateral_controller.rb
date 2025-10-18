ae48240abaab56dc6bc6f4bee48f970d28bdfbde
Restarone Solutions Inc. Software Engineering <engineering@restarone.solutions>
2025-10-18 12:43:10 -0400

    add promo info

--- app/controllers/comfy/admin/sales_collateral_controller.rb
+++ app/controllers/comfy/admin/sales_collateral_controller.rb
@@ -2,5 +2,12 @@ class Comfy::Admin::SalesCollateralController < Comfy::Admin::Cms::BaseControlle
   before_action :ensure_authority_to_manage_analytics
 
   def dashboard
+    # Get the host
+    subdomain = Apartment::Tenant.current
+    subdomain_resolved_for_apex = subdomain == 'public' || subdomain == 'root' ? "https://#{ENV['APP_HOST']}" : "https://#{subdomain}.#{ENV['APP_HOST']}" 
+    host = subdomain_resolved_for_apex
+    email_resolved_for_apex = subdomain == 'public' || subdomain == 'root' ? "public@#{ENV['APP_HOST']}" : "#{subdomain}@#{ENV['APP_HOST']}" 
+    @website = host
+    @email = email_resolved_for_apex
   end
 end
