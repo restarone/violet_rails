5714d36c3ea22837849a4c94fc155d7dbdd4456b
Restarone Solutions Inc. Software Engineering <engineering@restarone.solutions>
2025-11-06 11:29:34 -0500

    pass editor data to server

--- app/controllers/mailbox/email_templates_controller.rb
+++ app/controllers/mailbox/email_templates_controller.rb
@@ -1,4 +1,29 @@
 class Mailbox::EmailTemplatesController < Mailbox::BaseController
+
+  def index
+
+  end
+
   def new
+  end
+
+  def show
+  end
+
+  def create
+    html_doc = params[:htmldoc]
+    re_doc = params[:redoc]
+
+
+    render json: { id: 1 }
+  end
+
+  def edit
+    html_doc = params[:htmldoc]
+    re_doc = params[:redoc]
+  end
+
+  def destroy
+
   end
 end
