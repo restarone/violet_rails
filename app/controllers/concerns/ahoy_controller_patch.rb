9d4747ceb77364d7658009d36f306223a3e3bd71
Restarone Solutions Inc. Software Engineering <engineering@restarone.solutions>
2026-01-03 18:25:23 -0500

    track reverse ip when cookies accepted

--- app/controllers/concerns/ahoy_controller_patch.rb
+++ app/controllers/concerns/ahoy_controller_patch.rb
@@ -3,6 +3,7 @@ module AhoyControllerPatch
 		if Ahoy.cookies && tracking_enabled?
 			ahoy.set_visitor_cookie
 			ahoy.set_visit_cookie
+			ahoy.track('ip_reverse_lookup_domain', Reversed.lookup(request.ip))
 		else
 			# delete cookies if exist
 			ahoy.reset
